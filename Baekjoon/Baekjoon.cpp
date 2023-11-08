4350163a67b84241476b05afed67df42aa516d40
hayoonlee <htdobad@gmail.com>
2023-11-08 21:13:33 +0900

    백준/15552

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -5,21 +5,12 @@ int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	int arr[21];
+	int T, A, B;
+	cin >> T;
 
-	for (int i = 1; i <= 20; ++i)
-		arr[i] = i;
-
-	for (int i = 0; i < 10; ++i)
+	while (T--)
 	{
-		int a, b;
-		cin >> a >> b;
-
-		int c = (b - a + 1) / 2;
-		for (int j = 0; j < c; ++j)
-			swap(arr[a + j], arr[b - j]);
+		cin >> A >> B;
+		cout << A + B << '\n';
 	}
-
-	for (int i = 1; i <= 20; ++i)
-		cout << arr[i] << ' ';
 }
