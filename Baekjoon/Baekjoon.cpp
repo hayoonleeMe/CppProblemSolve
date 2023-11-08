e53d296ea11fcb70e4b82cc2a7d068026cee779f
hayoonlee <htdobad@gmail.com>
2023-11-08 21:10:18 +0900

    백준/10804

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,36 +1,25 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int arr[20];
-
-int Func(int interval, int cost)
-{
-	int ret = 0;
-	for (int i = 0; i < N; ++i)
-	{
-		int c = (arr[i] / interval + 1) * cost;
-		ret += c;
-	}
-
-	return ret;
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-	for (int i = 0; i < N; ++i)
-		cin >> arr[i];
+	int arr[21];
 
-	int Y = Func(30, 10);
-	int M = Func(60, 15);
+	for (int i = 1; i <= 20; ++i)
+		arr[i] = i;
 
-	if (Y < M)
-		cout << "Y " << Y;
-	else if (M < Y)
-		cout << "M " << M;
-	else
-		cout << "Y M " << Y;
+	for (int i = 0; i < 10; ++i)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		int c = (b - a + 1) / 2;
+		for (int j = 0; j < c; ++j)
+			swap(arr[a + j], arr[b - j]);
+	}
+
+	for (int i = 1; i <= 20; ++i)
+		cout << arr[i] << ' ';
 }
