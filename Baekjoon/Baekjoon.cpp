0259cdd76b2271611c2e8ac2d6dbd323268365a5
hayoonlee <htdobad@gmail.com>
2023-11-08 19:49:27 +0900

    백준/2752

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,33 +1,15 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-vector<int> arr;
-
 int main()
 {
-	cin.tie(NULL); cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
 
-	cin >> N;
-	if (N == 0)
-	{
-		cout << 0 << '\n';
-		return 0;
-	}
-
-	for (int i = 0; i < N; ++i)
-	{
-		int t;
-		cin >> t;
-		arr.push_back(t);
-	}
-	::sort(arr.begin(), arr.end());
-
-	float ret = 0.f;
-	int limit = int(N * 0.15 + 0.5f);
-	for (int i = limit; i < N - limit; ++i)
-		ret += arr[i];
-
-	cout << int(ret / (N - 2 * limit) + 0.5f) << '\n';
+	int arr[3];
+	for (int i = 0; i < 3; ++i)
+		cin >> arr[i];
+	sort(arr, arr + 3);
+	for (int i = 0; i < 3; ++i)
+		cout << arr[i] << ' ';
 }
