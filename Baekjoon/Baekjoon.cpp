bdf0d63f7bb71b32107ed80affeb4e0ab5caca42
hayoonlee <htdobad@gmail.com>
2023-11-08 19:58:25 +0900

    백준/2480

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -10,6 +10,17 @@ int main()
 	for (int i = 0; i < 3; ++i)
 		cin >> arr[i];
 	sort(arr, arr + 3);
-	for (int i = 0; i < 3; ++i)
-		cout << arr[i] << ' ';
+
+	int c = 1;
+	if (arr[1] == arr[0])
+		++c;
+	if (arr[1] == arr[2])
+		++c;
+
+	if (c == 3)
+		cout << 10000 + arr[1] * 1000;
+	else if (c == 2)
+		cout << 1000 + arr[1] * 100;
+	else if (c == 1)
+		cout << arr[2] * 100;
 }
