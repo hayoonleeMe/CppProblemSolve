787c77db62c6bfffbd5644d899f040c1f1399bc9
hayoonlee <htdobad@gmail.com>
2023-11-08 20:28:12 +0900

    백준/2309

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -5,14 +5,27 @@ int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-
-	int sum = 0;
-	int arr[5];
-	for (int i = 0; i < 5; ++i)
+	
+	int arr[9], sum = 0;
+	for (int i = 0; i < 9; ++i)
 	{
 		cin >> arr[i];
 		sum += arr[i];
 	}
-	sort(arr, arr + 5);
-	cout << sum / 5 << '\n' << arr[2];
+	sort(arr, arr + 9);
+
+	int i, j;
+	for (i = 0; i < 9; ++i)
+	{
+		for (j = i + 1; j < 9; ++j)
+		{
+			if (sum - arr[i] - arr[j] == 100)
+			{
+				for (int k = 0; k < 9; ++k)
+					if (k != i && k != j)
+						cout << arr[k] << '\n';
+				return 0;
+			}
+		}
+	}
 }
