5f9b6e2f526882da8dcd8b652a6dd43e1b977708
hayoonlee <htdobad@gmail.com>
2023-11-08 21:53:03 +0900

    백준/2562

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -5,12 +5,11 @@ int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	int T, A, B;
-	cin >> T;
-
-	while (T--)
-	{
-		cin >> A >> B;
-		cout << A + B << '\n';
-	}
+	int arr[9];
+	for (int i = 0; i < 9; ++i)
+		cin >> arr[i];
+	
+	// max_element
+	cout << *max_element(arr, arr + 9) << '\n';
+	cout << max_element(arr, arr + 9) - arr + 1;
 }
