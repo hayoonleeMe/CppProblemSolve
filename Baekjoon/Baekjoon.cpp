be787a06b2659d87bec5855432cbea7e1b93aa0c
hayoonlee <htdobad@gmail.com>
2023-11-09 12:58:53 +0900

    백준/10808

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,27 +1,24 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
+// 전역에 배열을 선언하면 0으로 초기화됨
+int alpha[26];
+
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	int arr[21];
+	/*int alpha[26];
+	int alpha[26] = {};
+	fill(alpha, alpha + 26, 0);
+	memset(alpha, 0, sizeof(alpha));*/
 
-	for (int i = 1; i <= 20; ++i)
-		arr[i] = i;
+	string s;
+	cin >> s;
 
-	for (int i = 0; i < 10; ++i)
-	{
-		int a, b;
-		cin >> a >> b;
+	for (int i = 0; i < s.size(); ++i)
+		++alpha[int(s[i] - 'a')];
 
-		// [a, b)
-		reverse(arr + a, arr + b + 1);
-		/*int c = (b - a + 1) / 2;
-		for (int j = 0; j < c; ++j)
-			swap(arr[a + j], arr[b - j]);*/
-	}
-
-	for (int i = 1; i <= 20; ++i)
-		cout << arr[i] << ' ';
+	for (int i = 0; i < 26; ++i)
+		cout << alpha[i] << ' ';
 }
