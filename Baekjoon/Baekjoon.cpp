f077bffd51ef8e1686493e53ec6a2fbee913bd1e
hayoonlee <htdobad@gmail.com>
2023-11-10 17:20:14 +0900

    백준/1919

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,48 +1,28 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int arr1[26];
-int arr2[26];
+int alpha1[26], alpha2[26];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
+	string s1, s2;
+	cin >> s1 >> s2;
 
-	for (int i = 0; i < N; ++i)
-	{
-		memset(arr1, 0, sizeof(arr1));
-		memset(arr2, 0, sizeof(arr2));
+	for (char c : s1)
+		++alpha1[c - 'a'];
 
-		string s1, s2;
-		cin >> s1 >> s2;
+	for (char c : s2)
+		++alpha2[c - 'a'];
 
-		if (s1.size() != s2.size())
-		{
-			cout << "Impossible\n";
-			continue;
-		}
+	int count = 0;
+	for (int i = 0; i < 26; ++i)
+		if (alpha1[i] >= alpha2[i])
+			count += alpha1[i] - alpha2[i];
+	for (int i = 0; i < 26; ++i)
+		if (alpha2[i] >= alpha1[i])
+			count += alpha2[i] - alpha1[i];
 
-		for (char c : s1)
-			++arr1[c - 'a'];
-
-		for (char c : s2)
-			++arr2[c - 'a'];
-
-		bool isPossible = true;
-		for (char c : s2)
-		{
-			if (arr2[c - 'a'] != arr1[c - 'a'])
-			{
-				cout << "Impossible\n";
-				isPossible = false;
-				break;
-			}
-		}
-
-		if (isPossible)
-			cout << "Possible\n";
-	}
+	cout << count;
 }
