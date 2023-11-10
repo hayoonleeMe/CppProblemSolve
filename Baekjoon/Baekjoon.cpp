7fe4b5d941533d68e024c369890abbdc7931abf2
hayoonlee <htdobad@gmail.com>
2023-11-10 15:32:57 +0900

    백준/3273

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,36 +1,31 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int arr[10];
+int N, X;
+int seq[100000];
+int arr[2000000];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N;
+	for (int i = 0; i < N; ++i)
+		cin >> seq[i];
+	cin >> X;
 
-	string s = to_string(N);
-	for (char c : s)
+	int count = 0;
+	for (int i = 0; i < N; ++i)
 	{
-		int n = c - '0';
-		if (n == 6)
-		{
-			if (arr[6] > arr[9])
-				++arr[9];
-			else
-				++arr[6];
-		}
-		else if (n == 9)
-		{
-			if (arr[9] > arr[6])
-				++arr[6];
-			else
-				++arr[9];
-		}
-		else
-			++arr[n];
+		int n = seq[i];
+		if (n >= X)
+			continue;
+
+		if (arr[X - n])
+			++count;
+
+		arr[n] = 1;
 	}
 
-	cout << *max_element(arr, arr + 10);
-}
+	cout << count;
+}	
