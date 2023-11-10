91c119425ea0a85af5b7364120f99c7eb9fa4c99
hayoonlee <htdobad@gmail.com>
2023-11-10 12:47:37 +0900

    백준/1475

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,23 +1,36 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int num[10];
+int N;
+int arr[10];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	int mul = 1;
-	for (int i = 0; i < 3; ++i)
-	{
-		int t; cin >> t;
-		mul *= t;
-	}
-	string s = to_string(mul);
+	cin >> N;
 
+	string s = to_string(N);
 	for (char c : s)
-		++num[c - '0'];
+	{
+		int n = c - '0';
+		if (n == 6)
+		{
+			if (arr[6] > arr[9])
+				++arr[9];
+			else
+				++arr[6];
+		}
+		else if (n == 9)
+		{
+			if (arr[9] > arr[6])
+				++arr[6];
+			else
+				++arr[9];
+		}
+		else
+			++arr[n];
+	}
 
-	for (int i = 0; i < 10; ++i)
-		cout << num[i] << '\n';
+	cout << *max_element(arr, arr + 10);
 }
