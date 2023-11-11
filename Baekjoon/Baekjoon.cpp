1ca0846dcf8e45540a87d5d3193442262a45e7c7
hayoonlee <htdobad@gmail.com>
2023-11-11 18:12:40 +0900

    백준/5397

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,32 +1,56 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, K;
-list<int> seq;
+int N;
+char BEGIN = '#';
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
-
-	cin >> N >> K;
-	for (int i = 1; i <= N; ++i)
-		seq.push_back(i);
 	
-	list<int>::iterator c = seq.begin();
-	cout << "<";
-	while (seq.size() > 1)
-	{
-		for (int j = 0; j < K - 1; ++j)
-		{
-			++c;
-			if (c == seq.end())
-				c = seq.begin();
-		}
+	cin >> N;
 
-		cout << *c << ", ";
-		c = seq.erase(c);
-		if (c == seq.end())
-			c = seq.begin();
+	while (N--)
+	{
+		string s;
+		cin >> s;
+
+		list<char> str;
+		str.push_back(BEGIN);
+		list<char>::iterator cursor = str.begin();
+
+		for (char c : s)
+		{
+			if (c == '<')
+			{
+				if (*cursor != BEGIN)
+					--cursor;
+			}
+			else if (c == '>')
+			{
+				if (cursor != --str.end())
+					++cursor;
+			}
+			else if (c == '-')
+			{
+				if (*cursor != BEGIN)
+				{
+					cursor = str.erase(cursor);
+					--cursor;
+				}
+			}
+			else
+			{
+				if (cursor != str.end())
+					++cursor;
+				str.insert(cursor, c);
+				--cursor;
+			}
+		}
+		str.pop_front();
+
+		for (auto c : str)
+			cout << c;
+		cout << '\n';
 	}
-	cout << *c << ">";
 }
