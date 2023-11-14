521f5369cbd8e628e7f047ed7373e5a6e471598f
hayoonlee <htdobad@gmail.com>
2023-11-14 17:05:30 +0900

    백준/1021

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,73 +1,58 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
+int N, M;
+int ans;
 deque<int> dq;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
+	cin >> N >> M;
+	for (int i = 1; i <= N; ++i)
+		dq.push_back(i);
 
-	string op;
-	while (N--)
+	int m;
+	while (M--)
 	{
-		cin >> op;
+		cin >> m;
 
-        int val;
-        if (op == "push_front")
-        {
-            cin >> val;
-            dq.push_front(val);
-        }
-        else if (op == "push_back")
-        {
-            cin >> val;
-            dq.push_back(val);
-        }
-        else if (op == "pop_front")
-        {
-            if (dq.empty())
-                cout << "-1\n";
-            else
-            {
-                cout << dq.front() << '\n';
-                dq.pop_front();
-            }
-        }
-        else if (op == "pop_back")
-        {
-            if (dq.empty())
-                cout << "-1\n";
-            else
-            {
-                cout << dq.back() << '\n';
-                dq.pop_back();
-            }
-        }
-        else if (op == "size")
-            cout << dq.size() << '\n';
-        else if (op == "empty")
-        {
-            if (dq.empty())
-                cout << "1\n";
-            else
-                cout << "0\n";
-        }
-        else if (op == "front")
-        {
-            if (dq.empty())
-                cout << "-1\n";
-            else
-                cout << dq.front() << '\n';
-        }
-        else if (op == "back")
-        {
-            if (dq.empty())
-                cout << "-1\n";
-            else
-                cout << dq.back() << '\n';
-        }
+		// 1
+		if (dq.front() == m)
+		{
+			dq.pop_front();
+			continue;
+		}
+
+		auto it = find(dq.begin(), dq.end(), m);
+		int op2 = it - dq.begin();
+		int op3 = dq.size() - op2;
+
+		// 2
+		if (op2 <= op3)
+		{
+			ans += op2;
+			for (int i = 0; i < op2; ++i)
+			{
+				dq.push_back(dq.front());
+				dq.pop_front();
+			}
+		}
+		// 3
+		else
+		{
+			ans += op3;
+			for (int i = 0; i < op3; ++i)
+			{
+				dq.push_front(dq.back());
+				dq.pop_back();
+			}
+		}
+
+		// 1
+		dq.pop_front();
 	}
+
+	cout << ans;
 }
