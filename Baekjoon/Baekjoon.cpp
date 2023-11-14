de155a3537124b00744e25df9b6b3c97ab104414
hayoonlee <htdobad@gmail.com>
2023-11-14 15:58:31 +0900

    백준/2164

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -2,58 +2,23 @@
 using namespace std;
 
 int N;
-queue<int> q;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N;
-	
-	string op;
-	while (N--)
+
+	queue<int> q;
+	for (int i = 1; i <= N; ++i)
+		q.push(i);
+
+	while (q.size() > 1)
 	{
-		cin >> op;
-
-		if (op == "push")
-		{
-			int val;
-			cin >> val;
-
-			q.push(val);
-		}
-		else if (op == "pop")
-		{
-			if (q.empty())
-				cout << "-1\n";
-			else
-			{
-				cout << q.front() << '\n';
-				q.pop();
-			}
-		}
-		else if (op == "size")
-			cout << q.size() << '\n';
-		else if (op == "empty")
-		{
-			if (q.empty())
-				cout << "1\n";
-			else
-				cout << "0\n";
-		}
-		else if (op == "front")
-		{
-			if (q.empty())
-				cout << "-1\n";
-			else
-				cout << q.front() << '\n';
-		}
-		else if (op == "back")
-		{
-			if (q.empty())
-				cout << "-1\n";
-			else
-				cout << q.back() << '\n';
-		}
+		q.pop();
+		q.push(q.front());
+		q.pop();
 	}
+
+	cout << q.front();
 }
