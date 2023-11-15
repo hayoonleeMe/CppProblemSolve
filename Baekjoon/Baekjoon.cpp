4472caa34aac3bd1478c2d79cfb720f0b0758d00
hayoonlee <htdobad@gmail.com>
2023-11-15 21:23:31 +0900

    백준/2504

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,39 +1,52 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int ans;
-
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	string str;
 	cin >> str;
-
 	stack<char> s;
-	int i = 0;
-	while (i < str.size())
+
+	int mul = 1, ans = 0;
+	for (int i = 0; i < str.size(); ++i)
 	{
-		if (str[i] == '(')
+		if (str[i] == '(' || str[i] == '[')
 		{
-			// 레이저
-			if (str[i + 1] == ')')
-			{
-				i += 2;
-				ans += s.size();
-				continue;
-			}
-			else
-				s.push(str[i]);
+			s.push(str[i]);
+			mul *= (str[i] == '(') ? 2 : 3;
 		}
 		else if (str[i] == ')')
 		{
-			s.pop();
-			++ans;
-		}
+			if (s.empty() || s.top() != '(')
+			{
+				cout << 0;
+				return 0;
+			}
 
-		++i;
+			if (str[i - 1] == '(')
+				ans += mul;
+			mul /= 2;
+			s.pop();
+		}
+		else if (str[i] == ']')
+		{
+			if (s.empty() || s.top() != '[')
+			{
+				cout << 0;
+				return 0;
+			}
+
+			if (str[i - 1] == '[')
+				ans += mul;
+			mul /= 3;
+			s.pop();
+		}
 	}
 
-	cout << ans;
+	if (s.empty())
+		cout << ans;
+	else
+		cout << 0;
 }
