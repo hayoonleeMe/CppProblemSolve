28d29f70a804505b9d53db8d15a4b0df6e2ff28c
hayoonlee <htdobad@gmail.com>
2023-11-15 13:58:58 +0900

    백준/4949

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,66 +1,45 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int T, N;
-
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> T;
-
-	while (T--)
+	string str;
+	for (getline(cin, str); str != "."; getline(cin, str))
 	{
-		string func;
-		cin >> func >> N;
-
-		// 문자열에서 숫자 분리
-		deque<int> dq;
-		string str, seq;
-		cin >> str;
-		str = str.substr(1, str.size() - 2);
-		istringstream ss(str);
-		while (getline(ss, seq, ','))
-			dq.push_back(stoi(seq));
+		stack<char> s;
 
 		bool isFailed = false;
-		bool isReversed = false;
-		for (char f : func)
+		for (char c : str)
 		{
-			if (f == 'R')
-				isReversed = !isReversed;
-			else if (f == 'D')
+			if (c == '(' || c == '[')
+				s.push(c);
+			else
 			{
-				if (dq.empty())
+				if (c == ')')
 				{
-					isFailed = true;
-					break;
+					if (s.empty() || s.top() != '(')
+					{
+						isFailed = true;
+						break;
+					}
+					s.pop();
 				}
-
-				if (isReversed)
-					dq.pop_back();
-				else
-					dq.pop_front();
+				else if (c == ']')
+				{
+					if (s.empty() || s.top() != '[')
+					{
+						isFailed = true;
+						break;
+					}
+					s.pop();
+				}
 			}
 		}
-
-		// 출력
-		if (dq.empty() && isFailed)
-			cout << "error\n";
+		if (isFailed || s.size() > 0)
+			cout << "no\n";
 		else
-		{
-			cout << '[';
-
-			if (!dq.empty())
-			{
-				if (isReversed)
-					reverse(dq.begin(), dq.end());
-				cout << dq.front();
-				for (auto it = dq.begin() + 1; it != dq.end(); ++it)
-					cout << ',' << *it;
-			}
-			
-			cout << "]\n";
-		}
+			cout << "yes\n";
 	}
 }
