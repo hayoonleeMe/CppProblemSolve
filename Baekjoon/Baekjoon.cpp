e9e246fd3f9a3fea355e7f2497c0b4235500cb6e
hayoonlee <htdobad@gmail.com>
2023-11-16 19:13:41 +0900

    백준/7569

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,63 +1,72 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-int M, N;
-int board[1000][1000];
-int dist[1000][1000];
-queue<pair<int, int>> q;
+#define Z 0
+#define X 1
+#define Y 2
+int dh[6] = { 1, -1, 0, 0, 0, 0 };
+int dx[6] = { 0, 0, 1, 0, -1, 0 };
+int dy[6] = { 0, 0, 0, 1, 0, -1 };
+int M, N, H;
+int board[100][100][100];
+int dist[100][100][100];
+queue<tuple<int, int, int>> q;
 int ans;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> M >> N;
-	for (int n = 0; n < N; ++n)
+	cin >> M >> N >> H;
+	for (int h = 0; h < H; ++h)
 	{
-		for (int m = 0; m < M; ++m)
+		for (int n = 0; n < N; ++n)
 		{
-			cin >> board[n][m];
-			if (board[n][m] == 1)
-				q.push({ n, m });
-			else if (board[n][m] == 0)
-				dist[n][m] = -1;
+			for (int m = 0; m < M; ++m)
+			{
+				cin >> board[h][n][m];
+				if (board[h][n][m] == 1)
+					q.push({ h,n,m });
+				else if (board[h][n][m] == 0)
+					dist[h][n][m] = -1;
+			}
 		}
 	}
 
 	while (!q.empty())
 	{
-		pair<int, int> cur = q.front();
+		tuple<int, int, int> cur = q.front();
 		q.pop();
 		
-		for (int i = 0; i < 4; ++i)
+		for (int i = 0; i < 6; ++i)
 		{
-			int nx = cur.X + dx[i];
-			int ny = cur.Y + dy[i];
+			int nh = get<Z>(cur) + dh[i];
+			int nx = get<X>(cur) + dx[i];
+			int ny = get<Y>(cur) + dy[i];
 
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+			if (nh < 0 || nh >= H || nx < 0 || nx >= N || ny < 0 || ny >= M)
 				continue;
-			if (dist[nx][ny] >= 0)
+			if (dist[nh][nx][ny] >= 0)
 				continue;
 
-			q.push({ nx, ny });
-			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+			q.push({ nh, nx, ny });
+			dist[nh][nx][ny] = dist[get<Z>(cur)][get<X>(cur)][get<Y>(cur)] + 1;
 		}
 	}
 
-	for (int n = 0; n < N; ++n)
+	for (int h = 0; h < H; ++h)
 	{
-		for (int m = 0; m < M; ++m)
+		for (int n = 0; n < N; ++n)
 		{
-			if (dist[n][m] == -1)
+			for (int m = 0; m < M; ++m)
 			{
-				cout << -1;
-				return 0;
+				if (dist[h][n][m] == -1)
+				{
+					cout << -1;
+					return 0;
+				}
+				ans = max(ans, dist[h][n][m]);
 			}
-			ans = max(ans, dist[n][m]);
 		}
 	}
 
