7132008e3a3e261efce8bd826cead3a99f356a7e
hayoonlee <htdobad@gmail.com>
2023-11-16 17:51:25 +0900

    백준/2178(답)

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -5,7 +5,6 @@ using namespace std;
 #define Y second
 int N, M;
 string board[100];
-int visited[100][100];
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
 queue<pair<int, int>> q;
@@ -17,35 +16,31 @@ int main()
 
 	cin >> N >> M;
 	for (int n = 0; n < N; ++n)
+	{
 		cin >> board[n];
+		fill(dist[n], dist[n] + M, -1);
+	}
 
-	visited[0][0] = 1;
 	q.push({ 0,0 });
-	dist[0][0] = 1;
-
+	dist[0][0] = 0;
 	while (!q.empty())
 	{
 		pair<int, int> cur = q.front();
 		q.pop();
-		if (cur.X == N - 1 && cur.Y == M - 1)
-			break;
-
 		for (int i = 0; i < 4; ++i)
 		{
 			int nx = cur.X + dx[i];
 			int ny = cur.Y + dy[i];
 
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M || board[nx][ny] != '1')
+			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+				continue;
+			if (dist[nx][ny] >= 0 || board[nx][ny] != '1')
 				continue;
 
-			if (!visited[nx][ny] || dist[cur.X][cur.Y] + 1 < dist[nx][ny])
-			{
-				visited[nx][ny] = 1;
-				q.push({ nx, ny });
-				dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-			}
+			q.push({ nx, ny });
+			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
 		}
 	}
 
-	cout << dist[N - 1][M - 1];
+	cout << dist[N - 1][M - 1] + 1;
 }
