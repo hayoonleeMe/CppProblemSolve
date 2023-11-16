236f6f4d68a3e573f155537a746d8deda3b10a40
hayoonlee <htdobad@gmail.com>
2023-11-16 23:26:49 +0900

    백준/1697

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,91 +1,37 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-int R, C;
-string board[1000];
-int fire[1000][1000];
-int dist[1000][1000];
-queue<pair<int, int>> F;
-queue<pair<int, int>> J;
+int N, K;
+int dist[100001];
+queue<int> q;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> R >> C;
-	for (int i = 0; i < R; ++i)
-	{
-		fill(fire[i], fire[i] + C, -1);
-		fill(dist[i], dist[i] + C, -1);
-		cin >> board[i];
-	}
+	cin >> N >> K;
+	memset(dist, -1, sizeof(dist));
 
-	for (int i = 0; i < R; ++i)
+	dist[N] = 0;
+	q.push(N);
+	while (!q.empty())
 	{
-		for (int j = 0; j < C; ++j)
+		int cur = q.front();
+		q.pop();
+
+		for (int nx : { cur - 1, cur + 1, 2 * cur })
 		{
-			if (board[i][j] == 'F')
-			{
-				fire[i][j] = 0;
-				F.push({ i, j });
-			}
-			else if (board[i][j] == 'J')
-			{
-				dist[i][j] = 0;
-				J.push({ i, j });
-			}
+			// K가 100,000까지라고 제한도 반드시 100,000까지일 필요는 없음 (2배씩 갈 수 있으므로)
+			// 그러나 이 문제에서는 100,000으로 제한해도 괜찮다. 하지만 다른 문제에서는 주의
+			if (nx < 0 || nx > 100000)
+				continue;
+			if (dist[nx] != -1)
+				continue;
+			
+			dist[nx] = dist[cur] + 1;
+			q.push(nx);
 		}
 	}
 
-	while (!F.empty())
-	{
-		pair<int, int> cur = F.front();
-		F.pop();
-
-		for (int i = 0; i < 4; ++i)
-		{
-			int nx = cur.X + dx[i];
-			int ny = cur.Y + dy[i];
-
-			if (nx < 0 || nx >= R || ny < 0 || ny >= C)
-				continue;
-			if (fire[nx][ny] >= 0 || board[nx][ny] == '#')
-				continue;
-
-			fire[nx][ny] = fire[cur.X][cur.Y] + 1;
-			F.push({ nx, ny });
-		}
-	}
-
-	while (!J.empty())
-	{
-		pair<int, int> cur = J.front();
-		J.pop();
-
-		for (int i = 0; i < 4; ++i)
-		{
-			int nx = cur.X + dx[i];
-			int ny = cur.Y + dy[i];
-
-			if (nx < 0 || nx >= R || ny < 0 || ny >= C)
-			{
-				cout << dist[cur.X][cur.Y] + 1;
-				return 0;
-			}
-
-			if (board[nx][ny] == '#' || dist[nx][ny] >= 0)
-				continue;
-			if (fire[nx][ny] != -1 && fire[nx][ny] <= dist[cur.X][cur.Y] + 1)
-				continue;
-
-			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-			J.push({ nx, ny });
-		}
-	}
-
-	cout << "IMPOSSIBLE";
+	cout << dist[K];
 }
