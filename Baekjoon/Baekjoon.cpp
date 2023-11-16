a17bbcc7765be1dac00cf70a6580b54726d0c543
hayoonlee <htdobad@gmail.com>
2023-11-16 17:46:45 +0900

    백준/2178

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -3,14 +3,13 @@ using namespace std;
 
 #define X first
 #define Y second
-
 int N, M;
-int board[500][500];
-int visited[500][500];
+string board[100];
+int visited[100][100];
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
 queue<pair<int, int>> q;
-int num, maxArea;
+int dist[100][100];
 
 int main()
 {
@@ -18,45 +17,35 @@ int main()
 
 	cin >> N >> M;
 	for (int n = 0; n < N; ++n)
-		for (int m = 0; m < M; ++m)
-			cin >> board[n][m];
+		cin >> board[n];
 
-	for (int n = 0; n < N; ++n)
+	visited[0][0] = 1;
+	q.push({ 0,0 });
+	dist[0][0] = 1;
+
+	while (!q.empty())
 	{
-		for (int m = 0; m < M; ++m)
+		pair<int, int> cur = q.front();
+		q.pop();
+		if (cur.X == N - 1 && cur.Y == M - 1)
+			break;
+
+		for (int i = 0; i < 4; ++i)
 		{
-			if (board[n][m] != 1 || visited[n][m])
+			int nx = cur.X + dx[i];
+			int ny = cur.Y + dy[i];
+
+			if (nx < 0 || nx >= N || ny < 0 || ny >= M || board[nx][ny] != '1')
 				continue;
 
-			++num;
-			visited[n][m] = 1;
-			q.push({ n, m });
-			int area = 1;
-
-			while (!q.empty())
+			if (!visited[nx][ny] || dist[cur.X][cur.Y] + 1 < dist[nx][ny])
 			{
-				pair<int, int> cur = q.front();
-				q.pop();
-
-				for (int i = 0; i < 4; ++i)
-				{
-					int nx = cur.X + dx[i];
-					int ny = cur.Y + dy[i];
-
-					if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-						continue;
-					if (visited[nx][ny] || board[nx][ny] != 1)
-						continue;
-
-					visited[nx][ny] = 1;
-					q.push({ nx, ny });
-					++area;
-				}
+				visited[nx][ny] = 1;
+				q.push({ nx, ny });
+				dist[nx][ny] = dist[cur.X][cur.Y] + 1;
 			}
-
-			maxArea = max(maxArea, area);
 		}
 	}
 
-	cout << num << '\n' << maxArea << '\n';
+	cout << dist[N - 1][M - 1];
 }
