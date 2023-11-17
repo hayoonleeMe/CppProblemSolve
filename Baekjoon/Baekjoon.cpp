797c1728be1db98bae832bd8eaf40efe50e39500
hayoonlee <htdobad@gmail.com>
2023-11-17 14:40:14 +0900

    백준/2667

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -4,41 +4,29 @@ using namespace std;
 #define Y second
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
-int N, M, K;
-int board[102][102];
-int dist[102][102];
+string board[27];
+int visited[27][27];
 queue<pair<int, int>> q;
-int num;
+int N, num;
 list<int> areas;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> M >> K;
-
+	cin >> N;
 	for (int i = 0; i < N; ++i)
-		fill(dist[i], dist[i] + M, -1);
-
-	for (int i = 0; i < K; ++i)
-	{
-		int m, n, mm, nn;
-		cin >> m >> n >> mm >> nn;
-
-		for (int k = n; k < nn; ++k)
-			for (int l = m; l < mm; ++l)
-				board[k][l] = -1;
-	}
+		cin >> board[i];
 
 	for (int i = 0; i < N; ++i)
 	{
-		for (int j = 0; j < M; ++j)
+		for (int j = 0; j < N; ++j)
 		{
-			if (board[i][j] == -1 || dist[i][j] >= 0)
+			if (board[i][j] != '1' || visited[i][j])
 				continue;
 
 			++num;
-			++dist[i][j];
+			visited[i][j] = 1;
 			q.push({ i, j });
 
 			int area = 0;
@@ -53,12 +41,12 @@ int main()
 					int nx = cur.X + dx[k];
 					int ny = cur.Y + dy[k];
 
-					if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+					if (nx < 0 || nx >= N || ny < 0 || ny >= N)
 						continue;
-					if (dist[nx][ny] >= 0 || board[nx][ny] == -1)
+					if (board[nx][ny] != '1' || visited[nx][ny])
 						continue;
 
-					dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+					visited[nx][ny] = 1;
 					q.push({ nx, ny });
 				}
 			}
@@ -67,8 +55,7 @@ int main()
 	}
 
 	areas.sort();
-	
 	cout << num << '\n';
 	for (int area : areas)
-		cout << area << ' ';
+		cout << area << '\n';
 }
