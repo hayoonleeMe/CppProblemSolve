ffd64bc009c836734630114ee96e924e05618f9e
hayoonlee <htdobad@gmail.com>
2023-11-17 13:52:36 +0900

    백준/10026

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -2,50 +2,78 @@
 using namespace std;
 #define X first
 #define Y second
-int dx[8] = { -1, -2, -2, -1, 1, 2, 2, 1 };
-int dy[8] = { -2, -1, 1, 2, 2, 1, -1, -2 };
-int T, I;
-int board[302][302];
-int dist[302][302];
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+int N, ans1, ans2;
+string board[102];
+int visited[102][102];
+
+void BFS(int i, int j, int& ans)
+{
+	++ans;
+	queue<pair<int, int>> q;
+	visited[i][j] = 1;
+	q.push({ i, j });
+
+	while (!q.empty())
+	{
+		pair<int, int> cur = q.front();
+		q.pop();
+
+		for (int k = 0; k < 4; ++k)
+		{
+			int nx = cur.X + dx[k];
+			int ny = cur.Y + dy[k];
+
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+				continue;
+			if (board[nx][ny] != board[i][j] || visited[nx][ny])
+				continue;
+
+			visited[nx][ny] = 1;
+			q.push({ nx, ny });
+		}
+	}
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> T;
-	while (T--)
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+		cin >> board[i];
+
+	// 정상
+	for (int i = 0; i < N; ++i)
 	{
-		cin >> I;
-		for (int i = 0; i < I; ++i)
-			fill(dist[i], dist[i] + I, -1);
-
-		queue<pair<int, int>> q;
-		int a, b, c, d;
-		cin >> a >> b >> c >> d;
-
-		dist[a][b] = 0;
-		q.push({ a, b });
-
-		while (!q.empty())
+		for (int j = 0; j < N; ++j)
 		{
-			pair<int, int> cur = q.front();
-			q.pop();
-
-			for (int i = 0; i < 8; ++i)
-			{
-				int nx = cur.X + dx[i];
-				int ny = cur.Y + dy[i];
-
-				if (nx < 0 || nx >= I || ny < 0 || ny >= I)
-					continue;
-				if (dist[nx][ny] >= 0)
-					continue;
-
-				dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-				q.push({ nx, ny });
-			}
+			if (visited[i][j])
+				continue;
+			BFS(i, j, ans1);
 		}
-
-		cout << dist[c][d] << '\n';
 	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		fill(visited[i], visited[i] + N, 0);
+
+		for (int j = 0; j < N; ++j)
+			if (board[i][j] == 'G')
+				board[i][j] = 'R';
+	}
+
+	// 적록색약
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			if (visited[i][j])
+				continue;
+			BFS(i, j, ans2);
+		}
+	}
+
+	cout << ans1 << ' ' << ans2;
 }
