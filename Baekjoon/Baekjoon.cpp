dc9a79febeb56fe67ddcd22d3b3165f222f45c19
hayoonlee <htdobad@gmail.com>
2023-11-17 15:30:34 +0900

    백준/5014

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,61 +1,40 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
-#define X first
-#define Y second
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-string board[27];
-int visited[27][27];
-queue<pair<int, int>> q;
-int N, num;
-list<int> areas;
+
+int F, S, G, U, D;
+int dist[1000002];
+queue<int> q;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-	for (int i = 0; i < N; ++i)
-		cin >> board[i];
+	cin >> F >> S >> G >> U >> D;
+	// [1, F]
+	fill(dist + 1, dist + F + 1, -1);
 
-	for (int i = 0; i < N; ++i)
+	++dist[S];
+	q.push(S);
+
+	while (!q.empty())
 	{
-		for (int j = 0; j < N; ++j)
+		int cur = q.front();
+		q.pop();
+			
+		for (int nx : { cur + U, cur - D })
 		{
-			if (board[i][j] != '1' || visited[i][j])
+			if (nx < 1 || nx > F)
+				continue;
+			if (dist[nx] >= 0)
 				continue;
 
-			++num;
-			visited[i][j] = 1;
-			q.push({ i, j });
-
-			int area = 0;
-			while (!q.empty())
-			{
-				++area;
-				pair<int, int> cur = q.front();
-				q.pop();
-
-				for (int k = 0; k < 4; ++k)
-				{
-					int nx = cur.X + dx[k];
-					int ny = cur.Y + dy[k];
-
-					if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-						continue;
-					if (board[nx][ny] != '1' || visited[nx][ny])
-						continue;
-
-					visited[nx][ny] = 1;
-					q.push({ nx, ny });
-				}
-			}
-			areas.push_back(area);
+			dist[nx] = dist[cur] + 1;
+			q.push(nx);
 		}
 	}
 
-	areas.sort();
-	cout << num << '\n';
-	for (int area : areas)
-		cout << area << '\n';
+	if (dist[G] == -1)
+		cout << "use the stairs";
+	else
+		cout << dist[G];
 }
