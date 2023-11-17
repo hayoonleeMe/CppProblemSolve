e9f3ba9d1892c0cdcec6973e6173578ca7792cf7
hayoonlee <htdobad@gmail.com>
2023-11-17 14:31:13 +0900

    백준/2583

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -4,76 +4,71 @@ using namespace std;
 #define Y second
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
-int N, ans1, ans2;
-string board[102];
-int visited[102][102];
-
-void BFS(int i, int j, int& ans)
-{
-	++ans;
-	queue<pair<int, int>> q;
-	visited[i][j] = 1;
-	q.push({ i, j });
-
-	while (!q.empty())
-	{
-		pair<int, int> cur = q.front();
-		q.pop();
-
-		for (int k = 0; k < 4; ++k)
-		{
-			int nx = cur.X + dx[k];
-			int ny = cur.Y + dy[k];
-
-			if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-				continue;
-			if (board[nx][ny] != board[i][j] || visited[nx][ny])
-				continue;
-
-			visited[nx][ny] = 1;
-			q.push({ nx, ny });
-		}
-	}
-}
+int N, M, K;
+int board[102][102];
+int dist[102][102];
+queue<pair<int, int>> q;
+int num;
+list<int> areas;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-	for (int i = 0; i < N; ++i)
-		cin >> board[i];
+	cin >> N >> M >> K;
 
-	// 정상
+	for (int i = 0; i < N; ++i)
+		fill(dist[i], dist[i] + M, -1);
+
+	for (int i = 0; i < K; ++i)
+	{
+		int m, n, mm, nn;
+		cin >> m >> n >> mm >> nn;
+
+		for (int k = n; k < nn; ++k)
+			for (int l = m; l < mm; ++l)
+				board[k][l] = -1;
+	}
+
 	for (int i = 0; i < N; ++i)
 	{
-		for (int j = 0; j < N; ++j)
+		for (int j = 0; j < M; ++j)
 		{
-			if (visited[i][j])
+			if (board[i][j] == -1 || dist[i][j] >= 0)
 				continue;
-			BFS(i, j, ans1);
+
+			++num;
+			++dist[i][j];
+			q.push({ i, j });
+
+			int area = 0;
+			while (!q.empty())
+			{
+				++area;
+				pair<int, int> cur = q.front();
+				q.pop();
+
+				for (int k = 0; k < 4; ++k)
+				{
+					int nx = cur.X + dx[k];
+					int ny = cur.Y + dy[k];
+
+					if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+						continue;
+					if (dist[nx][ny] >= 0 || board[nx][ny] == -1)
+						continue;
+
+					dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+					q.push({ nx, ny });
+				}
+			}
+			areas.push_back(area);
 		}
 	}
 
-	for (int i = 0; i < N; ++i)
-	{
-		fill(visited[i], visited[i] + N, 0);
-
-		for (int j = 0; j < N; ++j)
-			if (board[i][j] == 'G')
-				board[i][j] = 'R';
-	}
-
-	// 적록색약
-	for (int i = 0; i < N; ++i)
-	{
-		for (int j = 0; j < N; ++j)
-		{
-			if (visited[i][j])
-				continue;
-			BFS(i, j, ans2);
-		}
-	}
-
-	cout << ans1 << ' ' << ans2;
+	areas.sort();
+	
+	cout << num << '\n';
+	for (int area : areas)
+		cout << area << ' ';
 }
