8ede670b148e83b28afd4a51cb45abff2d5c69ea
hayoonlee <htdobad@gmail.com>
2023-11-19 20:58:39 +0900

    백준/1074 (답)

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -3,33 +3,22 @@ using namespace std;
 
 int N, r, c;
 
-void func(int i, int j, int n, int k)
+int func(int n, int r, int c)
 {
-	if (n == 1)
-	{
-		int nk = pow(2.0, n - 1) * pow(2.0, n - 1);
-		if (i == r && j == c)
-			cout << k + 0 * nk;
-		else if (i == r && j + 1 == c)
-			cout << k + 1 * nk;
-		else if (i + 1 == r && j == c)
-			cout << k + 2 * nk;
-		else if (i + 1 == r && j + 1 == c)
-			cout << k + 3 * nk;
-		return;
-	}
+	if (n == 0)
+		return 0;
 
-	int offset = pow(2.0, n - 1);
-	int nk = pow(2.0, n - 1) * pow(2.0, n - 1);
+	int half = 1<<(n - 1);
+	if (r < half && c < half)
+		return func(n - 1, r, c);
 
-	if (r < i + offset && c < j + offset)
-		func(i, j, n - 1, k + 0 * nk);
-	else if (r < i + offset && c >= j + offset)
-		func(i, j + offset, n - 1, k + 1 * nk);
-	else if (r >= i + offset && c < j + offset)
-		func(i + offset, j, n - 1, k + 2 * nk);
-	else if (r >= i + offset && c >= j + offset)
-		func(i + offset, j + offset, n - 1, k + 3 * nk);
+	if (r < half && c >= half)
+		return half * half + func(n - 1, r, c - half);
+
+	if (r >= half && c < half)
+		return 2 * half * half + func(n - 1, r - half, c);
+
+	return 3 * half * half + func(n - 1, r - half, c - half);
 }
 
 int main()
@@ -37,5 +26,5 @@ int main()
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N >> r >> c;
-	func(0, 0, N, 0);
+	cout << func(N, r, c);
 }
