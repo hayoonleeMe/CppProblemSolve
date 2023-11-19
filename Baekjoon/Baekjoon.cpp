a7c0e4d175f10888611cd8c7198e01133fcfc584
hayoonlee <htdobad@gmail.com>
2023-11-19 20:47:44 +0900

    백준/1074

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,27 +1,41 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
+int N, r, c;
 
-void func(int a, int b, int n)
+void func(int i, int j, int n, int k)
 {
 	if (n == 1)
 	{
-		cout << a << ' ' << b << '\n';
+		int nk = pow(2.0, n - 1) * pow(2.0, n - 1);
+		if (i == r && j == c)
+			cout << k + 0 * nk;
+		else if (i == r && j + 1 == c)
+			cout << k + 1 * nk;
+		else if (i + 1 == r && j == c)
+			cout << k + 2 * nk;
+		else if (i + 1 == r && j + 1 == c)
+			cout << k + 3 * nk;
 		return;
 	}
 
-	func(a, 6 - a - b, n - 1);
-	cout << a << ' ' << b << '\n';
-	func(6 - a - b, b, n - 1);
+	int offset = pow(2.0, n - 1);
+	int nk = pow(2.0, n - 1) * pow(2.0, n - 1);
+
+	if (r < i + offset && c < j + offset)
+		func(i, j, n - 1, k + 0 * nk);
+	else if (r < i + offset && c >= j + offset)
+		func(i, j + offset, n - 1, k + 1 * nk);
+	else if (r >= i + offset && c < j + offset)
+		func(i + offset, j, n - 1, k + 2 * nk);
+	else if (r >= i + offset && c >= j + offset)
+		func(i + offset, j + offset, n - 1, k + 3 * nk);
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-	// 1<<N : 2^N (1을 비트 기준 왼쪽으로 N칸 밀으라는 뜻 left shift)
-	cout << (1 << N) - 1 << '\n';
-	func(1, 3, N);
+	cin >> N >> r >> c;
+	func(0, 0, N, 0);
 }
