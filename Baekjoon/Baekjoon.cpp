966345ff5a794b7a10956943e5be846615bdf8ec
hayoonlee <htdobad@gmail.com>
2023-11-19 21:28:49 +0900

    백준/17478

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,30 +1,52 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, r, c;
+int N;
 
-int func(int n, int r, int c)
+void func(int n)
 {
-	if (n == 0)
-		return 0;
+	int dash = 4 * n;
 
-	int half = 1<<(n - 1);
-	if (r < half && c < half)
-		return func(n - 1, r, c);
+	
 
-	if (r < half && c >= half)
-		return half * half + func(n - 1, r, c - half);
+	for (int i = 0; i < dash; ++i)
+		cout << '_';
+	cout << "\"재귀함수가 뭔가요?\"\n";
 
-	if (r >= half && c < half)
-		return 2 * half * half + func(n - 1, r - half, c);
+	if (n == N)
+	{
+		for (int i = 0; i < dash; ++i)
+			cout << '_';
+		cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
+	}
+	else
+	{
+		for (int i = 0; i < dash; ++i)
+			cout << '_';
+		cout << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
 
-	return 3 * half * half + func(n - 1, r - half, c - half);
+		for (int i = 0; i < dash; ++i)
+			cout << '_';
+		cout << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
+
+		for (int i = 0; i < dash; ++i)
+			cout << '_';
+		cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+
+		func(n + 1);
+	}
+
+	for (int i = 0; i < dash; ++i)
+		cout << '_';
+	cout << "라고 답변하였지.\n";
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> r >> c;
-	cout << func(N, r, c);
+	cin >> N;
+
+	cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n";
+	func(0);
 }
