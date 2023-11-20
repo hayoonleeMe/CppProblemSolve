10d75ab8f477bc7c7b731fc5313dffaf3969f36d
hayoonlee <htdobad@gmail.com>
2023-11-20 14:23:11 +0900

    백준/1600

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,54 +1,75 @@
 ﻿#include <bits/stdc++.h>
+#include <random>
 using namespace std;
 
-int N;
-int board[2200][2200];
-int ans[3];
-
-void func(int n, int r1, int c1)
-{
-	if (n == 0)
-		return;
-
-	// 숫자가 같은지 확인
-	int t = board[r1][c1];
-	for (int i = r1; i < r1 + n; ++i)
-	{
-		for (int j = c1; j < c1 + n; ++j)
-		{
-			if (t != board[i][j])
-			{
-				int off = n / 3;
-				func(off, r1, c1);
-				func(off, r1, c1 + off);
-				func(off, r1, c1 + 2 * off);
-
-				func(off, r1 + off, c1);
-				func(off, r1 + off, c1 + off);
-				func(off, r1 + off, c1 + 2 * off);
-
-				func(off, r1 + 2 * off, c1);
-				func(off, r1 + 2 * off, c1 + off);
-				func(off, r1 + 2 * off, c1 + 2 * off);
-
-				return;
-			}
-		}
-	}
-
-	++ans[t + 1];
-}
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+int hdx[8] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+int hdy[8] = { -2, -1, 1, 2, 2, 1, -1, -2 };
+int K, W, H;
+int board[202][202];
+int dist[32][202][202];
+queue<tuple<int, int, int>> q;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < N; ++j)
+	cin >> K >> W >> H;
+	for (int i = 0; i < H; ++i)
+		for (int j = 0; j < W; ++j)
 			cin >> board[i][j];
+	
+	for (int k = 0; k <= K; ++k)
+		for (int i = 0; i < H; ++i)
+			fill(dist[k][i], dist[k][i] + W, -1);
 
-	func(N, 0, 0);
+	dist[0][0][0] = 0;
+	q.push({ 0, 0, 0 });
 
-	cout << ans[0] << '\n' << ans[1] << '\n' << ans[2];
+	while (!q.empty())
+	{
+		tuple<int, int, int> cur = q.front();
+		q.pop();
+
+		int k, h, w;
+		tie(k, h, w) = cur;
+
+		for (int i = 0; i < 4; ++i)
+		{
+			int nx = h + dx[i];
+			int ny = w + dy[i];
+
+			if (nx < 0 || nx >= H || ny < 0 || ny >= W || k > K)
+				continue;
+			if (dist[k][nx][ny] >= 0 || board[nx][ny] == 1)
+				continue;
+
+			dist[k][nx][ny] = dist[k][h][w] + 1;
+			q.push({ k, nx, ny });
+		}
+
+		for (int i = 0; i < 8; ++i)
+		{
+			int nx = h + hdx[i];
+			int ny = w + hdy[i];
+
+			if (nx < 0 || nx >= H || ny < 0 || ny >= W || k + 1 > K)
+				continue;
+			if (dist[k + 1][nx][ny] >= 0 || board[nx][ny] == 1)
+				continue;
+
+			dist[k + 1][nx][ny] = dist[k][h][w] + 1;
+			q.push({ k + 1, nx, ny });
+		}
+	}
+
+	int ans = 0x7f7f7f7f;
+	for (int k = 0; k <= K; ++k)
+		if (dist[k][H-1][W-1] != -1)
+			ans = min(ans, dist[k][H - 1][W - 1]);
+	if (ans != 0x7f7f7f7f)
+		cout << ans;
+	else
+		cout << -1;
 }
