bf16bd293070246c834a51dc8881d1f66d877118
hayoonlee <htdobad@gmail.com>
2023-11-20 16:57:16 +0900

    백준/1992

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 
 int N;
-int board[130][130];
-int ans[2];
+string board[64];
 
-bool allMatch(int n, int i, int j)
+bool chk(int n, int i, int j)
 {
-	int c = board[i][j];
 	for (int r = i; r < i + n; ++r)
-		for (int w = j; w < j + n; ++w)
-			if (board[r][w] != c)
+		for (int c = j; c < j + n; ++c)
+			if (board[r][c] != board[i][j])
 				return false;
 
 	return true;
@@ -18,20 +16,21 @@ bool allMatch(int n, int i, int j)
 
 void func(int n, int i, int j)
 {
-	if (n == 0)
-		return;
-
-	int c = board[i][j];
-	if (allMatch(n, i, j))
-		++ans[c];
-	else
+	if (chk(n, i, j))
 	{
-		int half = n / 2;
-		func(half, i, j);
-		func(half, i, j + half);
-		func(half, i + half, j);
-		func(half, i + half, j + half);
+		cout << board[i][j];
+		return;
 	}
+
+	cout << '(';
+
+	int half = n / 2;
+	func(half, i, j);
+	func(half, i, j + half);
+	func(half, i + half, j);
+	func(half, i + half, j + half);
+
+	cout << ')';
 }
 
 int main()
@@ -40,10 +39,7 @@ int main()
 
 	cin >> N;
 	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < N; ++j)
-			cin >> board[i][j];
+		cin >> board[i];
 
 	func(N, 0, 0);
-
-	cout << ans[0] << '\n' << ans[1];
 }
