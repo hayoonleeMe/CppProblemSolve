fa36d9b2763c4f9cfed1ff24e34e80c2a8006189
hayoonlee <htdobad@gmail.com>
2023-11-21 13:48:09 +0900

    백준/9663 (답)

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,34 +1,45 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, S;
-int arr[22];
-int cnt = 0;
+int N;
+const int MAX = 14;
+bool isused1[2 * MAX + 2];
+bool isused2[2 * MAX + 2];
+bool isused3[2 * MAX + 2];
 
-void func(int cur, int tot)
+int cnt = 0;
+void func(int cur)
 {
 	if (cur == N)
 	{
-		if (tot == S)
-			++cnt;
+		++cnt;
 		return;
 	}
 
-	func(cur + 1, tot);
-	func(cur + 1, tot + arr[cur]);
+	for (int i = 0; i < N; ++i)
+	{
+		if (isused1[i] || isused2[cur + i] || isused3[cur - i + N - 1])
+			continue;
+
+		isused1[i] = 1;
+		isused2[cur + i] = 1;
+		isused3[cur - i + N - 1] = 1;
+
+		func(cur + 1);
+
+		isused1[i] = 0;
+		isused2[cur + i] = 0;
+		isused3[cur - i + N - 1] = 0;
+	}
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> S;
-	for (int i = 0; i < N; ++i)
-		cin >> arr[i];
+	cin >> N;
 
-	func(0, 0);
+	func(0);
 
-	if (S == 0)
-		--cnt;
 	cout << cnt;
 }
