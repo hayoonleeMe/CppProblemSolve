8747f73ead725e4e7d5ef00ad0aef126b894df09
hayoonlee <htdobad@gmail.com>
2023-11-22 19:29:57 +0900

    백준/6603 (별해)

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,28 +1,29 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, M;
-int arr[10];
-int mask[10];
+int K;
+int arr[15];
+int mask[15];
 
 // next_permutation 사용
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> M;
-	for (int i = 0; i < N; ++i)
-		cin >> arr[i];
-	sort(arr, arr + N);
-
-	for (int i = M; i < N; ++i)
-		mask[i] = 1;
-
-	do
+	for (cin >> K; K != 0; cin >> K)
 	{
-		for (int i = 0; i < N; ++i)
-			if (!mask[i])
-				cout << arr[i] << ' ';
+		for (int i = 0; i < K; ++i)
+			cin >> arr[i];
+		for (int i = 6; i < K; ++i)
+			mask[i] = 1;
+
+		do
+		{
+			for (int i = 0; i < K; ++i)
+				if (!mask[i])
+					cout << arr[i] << ' ';
+			cout << '\n';
+		} while (next_permutation(mask, mask + K));
 		cout << '\n';
-	} while (next_permutation(mask, mask + N));
+	}
 }
