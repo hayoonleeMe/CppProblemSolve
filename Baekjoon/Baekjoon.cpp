bf699aa1c202f73d0d7276d6fe1723581692054b
hayoonlee <htdobad@gmail.com>
2023-11-22 14:06:57 +0900

    백준/17219

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,21 +1,25 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int p[1002];
+int N, M;
+map<string, string> m;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-	for (int i = 0; i < N; ++i)
-		cin >> p[i];
-	sort(p, p + N);
+	cin >> N >> M;
+	while (N--)
+	{
+		string a, b;
+		cin >> a >> b;
+		m[a] = b;
+	}
 
-	int sum = 0;
-	for (int i = 0; i < N; ++i)
-		sum += p[i] * (N - i);
-
-	cout << sum;
+	while (M--)
+	{
+		string s;
+		cin >> s;
+		cout << m[s] << '\n';
+	}
 }
