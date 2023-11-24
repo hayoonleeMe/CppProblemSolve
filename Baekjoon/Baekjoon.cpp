d2f5e0275560bddee4a68338b6a6e5d047f12bd2
hayoonlee <htdobad@gmail.com>
2023-11-24 18:11:44 +0900

    백준/16987 (답)

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,69 +1,63 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int dx[4] = { -1, 0, 1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-bool mask[25];
-string board[5];
-int ans;
-queue<pair<int, int>> q;
+#define HP first
+#define W second
 
-// next_permutation 사용
+int N, ans, cnt;
+pair<int, int> arr[10];
+
+void func(int k)
+{
+	if (k == N)
+	{
+		ans = max(ans, cnt);
+		return;
+	}
+
+	if (arr[k].HP <= 0 || cnt == N - 1)
+	{
+		func(k + 1);
+		return;
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		if (i == k || arr[i].HP <= 0)
+			continue;
+
+		arr[k].HP -= arr[i].W;
+		arr[i].HP -= arr[k].W;	
+		if (arr[k].HP <= 0)
+			++cnt;
+		if (arr[i].HP <= 0)
+			++cnt;
+
+		func(k + 1);
+
+		if (arr[k].HP <= 0)
+			--cnt;
+		if (arr[i].HP <= 0)
+			--cnt;
+		arr[k].HP += arr[i].W;
+		arr[i].HP += arr[k].W;
+	}
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	for (int i = 0; i < 5; ++i)
-		cin >> board[i];
+	cin >> N;
 
-	fill(mask + 7, mask + 25, true);
-
-	do
+	for (int i = 0; i < N; ++i)
 	{
-		bool seq[5][5] = {}, visited[5][5] = {};
-		for (int i = 0; i < 25; ++i)
-		{
-			if (!mask[i])
-			{
-				int x = i / 5, y = i % 5;
-				seq[x][y] = 1;
+		int s, w;
+		cin >> s >> w;
+		arr[i] = { s, w };
+	}
 
-				if (q.empty())
-				{
-					visited[x][y] = 1;
-					q.push({ x, y });
-				}
-			}
-		}
-
-		int ns = 0, adj = 0;
-		while (!q.empty())
-		{
-			int x, y;
-			tie(x, y) = q.front();
-			q.pop();
-
-			++adj;
-			ns += (board[x][y] == 'S');
-
-			for (int i = 0; i < 4; ++i)
-			{
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-
-				if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5)
-					continue;
-				if (visited[nx][ny] == 1 || seq[nx][ny] == 0)
-					continue;
-
-				visited[nx][ny] = 1;
-				q.push({ nx, ny });
-			}
-		}
-
-		ans += (adj >= 7 && ns >= 4);
-			
-	} while (next_permutation(mask, mask + 25));
+	func(0);
 
 	cout << ans;
 }
