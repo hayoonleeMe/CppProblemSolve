c2c60c438d977f1a3c121a80512bdc4070677e1c
hayoonlee <htdobad@gmail.com>
2023-11-25 15:39:25 +0900

    백준/2910

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,24 +1,42 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N; 
-vector<long long> v;
+int N, C;
+vector<pair<int, int>> v;
+
+bool cmp(const pair<int, int>& a, const pair<int, int>& b)
+{
+	return a.second > b.second;
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
+	cin >> N >> C;
 	for (int i = 0; i < N; ++i)
 	{
-		string s;
-		cin >> s;
-		reverse(s.begin(), s.end());
-		// 변환한 값이 정수 범위를 초과 한다면 std::out_or_range 를 던진다.
-		v.push_back(stoll(s));
+		int n;
+		cin >> n;
+
+		bool find = false;
+		for (auto& p : v)
+		{
+			if (p.first == n)
+			{
+				++p.second;
+				find = true;
+				break;
+			}
+		}
+		if (!find)
+			v.push_back({ n, 1 });
 	}
 
-	sort(v.begin(), v.end());
-	for (long long n : v)
-		cout << n << '\n';
+	stable_sort(v.begin(), v.end(), cmp);
+	for (auto p : v)
+	{
+		while (p.second--)
+			cout << p.first << ' ';
+	}
 }
