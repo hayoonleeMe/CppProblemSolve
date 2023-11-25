fbf1c72d2c54c2a06bdb0e03802c2510a5f335dc
hayoonlee <htdobad@gmail.com>
2023-11-25 19:19:30 +0900

    백준/1149

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,30 +1,38 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
+#define R 0
+#define G 1
+#define B 2
 int N;
-int s[305];
-int d[305];
+int r[1005];
+int g[1005];
+int b[1005];
+int d[1005][3];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N;
-	int tot = 0;
 	for (int i = 1; i <= N; ++i)
 	{
-		cin >> s[i];
-		tot += s[i];
+		int rc, gc, bc;
+		cin >> rc >> gc >> bc;
+		r[i] = rc;	g[i] = gc;	b[i] = bc;
 	}
 
+	d[1][R] = r[1];
+	d[1][G] = g[1];
+	d[1][B] = b[1];
 
-	d[1] = s[1];
-	d[2] = s[2];
-	d[3] = s[3];
+	for (int i = 2; i <= N; ++i)
+	{
+		d[i][R] = r[i] + min(d[i - 1][G], d[i - 1][B]);
+		d[i][G] = g[i] + min(d[i - 1][R], d[i - 1][B]);
+		d[i][B] = b[i] + min(d[i - 1][R], d[i - 1][G]);
+	}
 
-	for (int i = 4; i < N; ++i)
-		d[i] = s[i] + min(d[i - 2], d[i - 3]);
-
-	int ans = tot - min(d[N - 1], d[N - 2]);
+	int ans = min({ d[N][R], d[N][G], d[N][B] });
 	cout << ans;
 }
