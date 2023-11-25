2c8588b7f5345842ad5d97cdd691c49690758d4e
hayoonlee <htdobad@gmail.com>
2023-11-25 16:06:22 +0900

    백준/7795

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,41 +1,48 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-vector<tuple<string, int, int, int>> v;
-
-bool cmp(const tuple<string, int, int, int>& x, const tuple<string, int, int, int>& y)
-{
-	string xn, yn;
-	int xa, xb, xc, ya, yb, yc;
-	tie(xn, xa, xb, xc) = x;
-	tie(yn, ya, yb, yc) = y;
-
-	if (xa != ya)
-		return xa > ya;
-	if (xb != yb)
-		return xb < yb;
-	if (xc != yc)
-		return xc > yc;
-	return xn < yn;
-}
+int T, N, M;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-	for (int i = 0; i < N; ++i)
+	cin >> T;
+	while (T--)
 	{
-		string s;
-		int a, b, c;
-		cin >> s >> a >> b >> c;
-		v.push_back({ s, a, b, c });
-	}
+		cin >> N >> M;
+		vector<int> vn, vm;
+		for (int i = 0; i < N; ++i)
+		{
+			int t;
+			cin >> t;
+			vn.push_back(t);
+		}
+		for (int i = 0; i < M; ++i)
+		{
+			int t;
+			cin >> t;
+			vm.push_back(t);
+		}
 
-	sort(v.begin(), v.end(), cmp);
-	for (auto t : v)
-	{
-		cout << get<0>(t) << '\n';
+		sort(vn.begin(), vn.end(), greater<int>());
+		sort(vm.begin(), vm.end(), greater<int>());
+
+		int cnt = 0;
+		int mx_vm_i = 0;
+		int vm_size = vm.size();
+		int i = 0;
+		while (i < N && mx_vm_i < M)
+		{
+			if (vn[i] > vm[mx_vm_i])
+			{
+				cnt += vm_size - mx_vm_i;
+				++i;
+			}
+			else
+				++mx_vm_i;
+		}
+
+		cout << cnt << '\n';
 	}
 }
