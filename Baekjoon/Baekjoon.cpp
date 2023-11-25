03a15fab751d8cdc1e23c0c3f03a6213897fbaeb
hayoonlee <htdobad@gmail.com>
2023-11-25 18:15:28 +0900

    백준/2579 (별해)

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -3,26 +3,28 @@ using namespace std;
 
 int N;
 int s[305];
-int d[305][3];
+int d[305];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N;
+	int tot = 0;
 	for (int i = 1; i <= N; ++i)
-		cin >> s[i];
-
-	d[1][1] = s[1]; 
-	d[1][2] = s[1];
-	d[2][1] = s[2];
-	d[2][2] = s[2] + s[1];
-
-	for (int i = 3; i <= N; ++i)
 	{
-		d[i][2] = d[i - 1][1] + s[i];
-		d[i][1] = s[i] + max(d[i - 2][1], d[i - 2][2]);
+		cin >> s[i];
+		tot += s[i];
 	}
 
-	cout << max(d[N][1], d[N][2]);
+
+	d[1] = s[1];
+	d[2] = s[2];
+	d[3] = s[3];
+
+	for (int i = 4; i < N; ++i)
+		d[i] = s[i] + min(d[i - 2], d[i - 3]);
+
+	int ans = tot - min(d[N - 1], d[N - 2]);
+	cout << ans;
 }
