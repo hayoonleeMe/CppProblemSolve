78b3e58154556aed55e886135b611e7f8a8802cf
hayoonlee <htdobad@gmail.com>
2023-11-26 21:22:45 +0900

    백준/11727

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -2,22 +2,17 @@
 using namespace std;
 
 int N;
-int arr[502][502];
-int d[502][502];
+const int mod = 10007;
+int d[1005];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
-
 	cin >> N;
-	for (int i = 1; i <= N; ++i)
-		for (int j = 1; j <= i; ++j)
-			cin >> arr[i][j];
 
-	d[1][1] = arr[1][1];
-	for (int i = 2; i <= N; ++i)
-		for (int j = 1; j <= i; ++j)
-			d[i][j] = arr[i][j] + max(d[i - 1][j], d[i - 1][j - 1]);
-
-	cout << *max_element(d[N] + 1, d[N] + N + 1);
+	d[1] = 1;
+	d[2] = 3;
+	for (int i = 3; i <= N; ++i)
+		d[i] = (d[i - 1] + d[i - 2] * 2) % mod;
+	cout << d[N];
 }
