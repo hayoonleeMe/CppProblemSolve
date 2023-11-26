4a077644b8cdd9e5bdaffd58f7d082d798ad04e4
hayoonlee <htdobad@gmail.com>
2023-11-26 21:13:54 +0900

    백준/1932

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -2,45 +2,22 @@
 using namespace std;
 
 int N;
-int d[1000005];
-int pre[1000005];
+int arr[502][502];
+int d[502][502];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N;
+	for (int i = 1; i <= N; ++i)
+		for (int j = 1; j <= i; ++j)
+			cin >> arr[i][j];
 
+	d[1][1] = arr[1][1];
 	for (int i = 2; i <= N; ++i)
-	{
-		d[i] = d[i - 1] + 1;
-		pre[i] = i - 1;
+		for (int j = 1; j <= i; ++j)
+			d[i][j] = arr[i][j] + max(d[i - 1][j], d[i - 1][j - 1]);
 
-		if (i % 2 == 0)
-		{
-			if (d[i / 2] + 1 < d[i])
-			{
-				pre[i] = i / 2;
-				d[i] = d[i / 2] + 1;
-			}
-		}
-		if (i % 3 == 0)
-		{
-			if (d[i / 3] + 1 < d[i])
-			{
-				pre[i] = i / 3;
-				d[i] = d[i / 3] + 1;
-			}
-		}
-	}
-
-	cout << d[N] << '\n';
-	int i = N;
-	while (1)
-	{
-		cout << i << ' ';
-		if (i == 1)
-			break;
-		i = pre[i];
-	}
+	cout << *max_element(d[N] + 1, d[N] + N + 1);
 }
