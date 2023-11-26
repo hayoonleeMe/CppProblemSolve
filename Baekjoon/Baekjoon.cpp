955d04b11b67ab49f4c6af40f889cc7889801a6c
hayoonlee <htdobad@gmail.com>
2023-11-27 00:31:35 +0900

    백준/2193

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int N;
-const int mod = 10007;
-int d[1005];
+long long d[100];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
-	cin >> N;
 
+	cin >> N;
 	d[1] = 1;
-	d[2] = 3;
+	d[2] = 1;
+	
 	for (int i = 3; i <= N; ++i)
-		d[i] = (d[i - 1] + d[i - 2] * 2) % mod;
+		d[i] = d[i - 1] + d[i - 2];
 	cout << d[N];
 }
