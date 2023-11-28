ef2f0482c1833f115b041682497f44fed3123b07
hayoonlee <htdobad@gmail.com>
2023-11-28 22:19:24 +0900

    백준/10844 (답)

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,18 +1,36 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
+const int mod = 1000000000;
 int N;
-long long d[100];
+int d[105][10];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N;
-	d[0] = 0;
-	d[1] = 1;
-	for (int i = 2; i <= N; ++i)
-		d[i] = d[i - 1] + d[i - 2];
+	for (int k = 1; k <= 9; ++k)
+		d[1][k] = 1;
 
-	cout << d[N];
+	for (int i = 2; i <= N; ++i)
+	{
+		for (int k = 0; k <= 9; ++k)
+		{
+			if (k != 0)
+				d[i][k] += d[i - 1][k - 1];
+			if (k != 9)
+				d[i][k] += d[i - 1][k + 1];
+			d[i][k] %= mod;
+		}
+	}
+
+	int ans = 0;
+	for (int k = 0; k <= 9; ++k)
+	{
+		ans += d[N][k];
+		ans %= mod;
+	}
+
+	cout << ans;
 }
