44846461cb7a74092ece068e40dc6b07ae18fc24
hayoonlee <htdobad@gmail.com>
2023-11-29 23:40:22 +0900

    백준/2156 (답)

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int N;
 int arr[10005];
-int d[10005][3];
+int d[10005];
 
 int main()
 {
@@ -13,20 +13,13 @@ int main()
 	for (int i = 1; i <= N; ++i)
 		cin >> arr[i];
 
-	d[1][1] = arr[1];
-	d[1][2] = arr[1];
+	d[1] = arr[1];
+	d[2] = arr[1] + arr[2];
 
-	for (int i = 2; i <= N; ++i)
+	for (int i = 3; i <= N; ++i)
 	{
-		d[i][1] = arr[i];
-		for (int j = i - 2; j > 0; --j)
-			d[i][1] = max({ d[i][1], arr[i] + d[j][1], arr[i] + d[j][2] });
-		d[i][2] = arr[i] + d[i - 1][1];
+		d[i] = max({ d[i - 1], d[i - 2] + arr[i], d[i - 3] + arr[i - 1] + arr[i] }); 
 	}
 
-	int ans = 0;
-	for (int i = 1; i <= N; ++i)
-		for (int j = 1; j <= 2; ++j)
-			ans = max(ans, d[i][j]);
-	cout << ans;
+	cout << d[N];
 }
