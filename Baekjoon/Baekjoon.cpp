87587635c3adde1f20609420f3786a252d9f4620
hayoonlee <htdobad@gmail.com>
2023-11-30 18:27:56 +0900

    백준/11057

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,24 +1,35 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
+const int mod = 10007;
 int N;
-int arr[1005];
-int d[1005];
+int d[1005][10];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N;
-	for (int i = 1; i <= N; ++i)
-	{
-		cin >> arr[i];
-		d[i] = arr[i];
-	}
+
+	for (int i = 0; i <= 9; ++i)
+		d[1][i] = 1;
 
 	for (int i = 2; i <= N; ++i)
-		for (int j = i - 1; j >= 1; --j)
-			d[i] = max(d[i], d[j] + arr[i - j]);
+	{
+		for (int j = 0; j <= 9; ++j)
+		{
+			int sum = 0;
+			for (int k = j; k <= 9; ++k)
+				sum = (sum + d[i - 1][k]) % mod;
+			d[i][j] = sum;
 
-	cout << d[N];
+		}
+		/*for (int j = 1; j <= 9; ++j)
+			d[i][j] = d[i][j - 1] - d[i - 1][j - 1];*/
+	}
+
+	int ans = 0;
+	for (int i = 0; i <= 9; ++i)
+		ans = (ans + d[N][i]) % mod;
+	cout << ans;
 }
