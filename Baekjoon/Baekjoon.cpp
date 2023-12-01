92cafa1f2e5266f91983375bb295d6984144c732
hayoonlee <htdobad@gmail.com>
2023-12-01 20:40:15 +0900

    백준/9465

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,35 +1,36 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-const int mod = 10007;
-int N;
-int d[1005][10];
+int T, N;
+int arr[3][100005];
+int d[3][100005];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-
-	for (int i = 0; i <= 9; ++i)
-		d[1][i] = 1;
-
-	for (int i = 2; i <= N; ++i)
+	cin >> T;
+	while (T--)
 	{
-		for (int j = 0; j <= 9; ++j)
+		cin >> N;
+
+		for (int i = 0; i < 3; ++i)
+			fill(d[i], d[i] + N, 0);
+
+		for (int i = 1; i <= 2; ++i)
+			for (int j = 1; j <= N; ++j)
+				cin >> arr[i][j];
+
+		for (int i = 1; i <= N; ++i)
 		{
-			int sum = 0;
-			for (int k = j; k <= 9; ++k)
-				sum = (sum + d[i - 1][k]) % mod;
-			d[i][j] = sum;
-
+			d[0][i] = max({ d[0][i - 1], d[1][i - 1], d[2][i - 1] });
+			d[1][i] = arr[1][i] + max(d[0][i - 1], d[2][i - 1]);
+			d[2][i] = arr[2][i] + max(d[0][i - 1], d[1][i - 1]);
 		}
-		/*for (int j = 1; j <= 9; ++j)
-			d[i][j] = d[i][j - 1] - d[i - 1][j - 1];*/
-	}
 
-	int ans = 0;
-	for (int i = 0; i <= 9; ++i)
-		ans = (ans + d[N][i]) % mod;
-	cout << ans;
+		int ans = 0;
+		for (int i = 0; i < 3; ++i)
+			ans = max(ans, d[i][N]);
+		cout << ans << '\n';
+	}
 }
