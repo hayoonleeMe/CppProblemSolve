09a4837b814f8c9f830605beb3fff69f424b07ad
hayoonlee <htdobad@gmail.com>
2023-12-02 17:33:40 +0900

    백준/14002

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,22 +1,48 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, K;
-int a[105];
-int d[10005];
+int N;
+int a[1005];
+int d[1005];
+int idx[1005];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> K;
-	for (int i = 0; i < N; ++i)
+	cin >> N;
+	for (int i = 1; i <= N; ++i)
 		cin >> a[i];
+	fill(d, d + N + 1, 1);
 
-	d[0] = 1;
-	for (int i = 0; i < N; ++i)
-		for (int j = a[i]; j <= K; ++j)
-			d[j] += d[j - a[i]];
+	for (int i = 2; i <= N; ++i)
+	{
+		for (int j = i - 1; j > 0; --j)
+		{
+			if (a[i] > a[j])
+			{
+				if (d[i] < d[j] + 1)
+				{
+					d[i] = d[j] + 1;
+					idx[i] = j;
+				}
+			}
+		}
+	}
 
-	cout << d[K];
+	int* last = max_element(d + 1, d + N + 1);
+	cout << *last << '\n';
+
+	stack<int> s;
+	int lastIdx = last - d;
+	while (lastIdx != 0)
+	{
+		s.push(a[lastIdx]);
+		lastIdx = idx[lastIdx];
+	}
+	while (!s.empty())
+	{
+		cout << s.top() << ' ';
+		s.pop();
+	}
 }
