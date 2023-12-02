7b3a5d6f050736edd5aa724a7aea06542211e438
hayoonlee <htdobad@gmail.com>
2023-12-02 18:43:52 +0900

    백준/1699 (답)

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -10,21 +10,11 @@ int main()
 
 	cin >> N;
 
-	int p = 0;
 	for (int i = 1; i <= N; ++i)
 	{
-		if (i >= (p + 1) * (p + 1))
-		{
-			++p;
-			d[p * p] = 1;
-			continue;
-		}
-
-		int m = 0x7f7f7f7f;
-		for (int j = p; j > 0; --j)
-			m = min(m, (i / (j * j)) * d[j * j] + d[(i % (j * j))]);
-		if (m != 0x7f7f7f7f)
-			d[i] = m;
+		d[i] = i;
+		for (int j = 1; j * j <= i; ++j)
+			d[i] = min(d[i], d[i - j * j] + 1);
 	}
 
 	cout << d[N];
