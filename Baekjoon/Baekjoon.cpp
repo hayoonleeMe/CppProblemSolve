e09334cc36a248f61fe40d5dfe7e53785451be77
hayoonlee <htdobad@gmail.com>
2023-12-04 16:04:07 +0900

    백준/9084

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,26 +1,33 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int d[1005][1005];
-string A, B;
+int T, N, M;
+int a[25];
+int d[10005];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> A >> B;
-	int an = A.size(), bn = B.size();
-
-	for (int i = 1; i <= an; ++i)
+	cin >> T;
+	while (T--)
 	{
-		for (int j = 1; j <= bn; ++j)
-		{
-			if (A[i - 1] == B[j - 1])
-				d[i][j] = d[i - 1][j - 1] + 1;
-			else
-				d[i][j] = max(d[i - 1][j], d[i][j - 1]);
-		}
-	}
+		cin >> N;
+		for (int i = 0; i < N; ++i)
+			cin >> a[i];
+		cin >> M;
+		fill(d + 1, d + M + 1, 0);
+		d[0] = 1;
 
-	cout << d[an][bn];
+		for (int i = 0; i < N; ++i)
+		{
+			int c = a[i];
+			for (int j = c; j <= M; ++j)
+			{
+				d[j] += d[j - c];
+			}
+		}
+
+		cout << d[M] << '\n';
+	}
 }
