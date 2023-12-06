e28b4e0e8808ca8b72e30918f68e3bd16d95ea5a
hayoonlee <htdobad@gmail.com>
2023-12-06 23:14:10 +0900

    백준/9657 (답)

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,29 +1,31 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, M, ans;
-int d[1005][1005];
-string a[1005];
+int N;
+int d[1005];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> M;
-	for (int i = 0; i < N; ++i)
-		cin >> a[i];
+	cin >> N;
+	d[1] = 0;
+	d[2] = 1;
+	d[3] = 0;
+	d[4] = 0;
 
-	for (int i = 1; i <= N; ++i)
+	for (int i = 5; i <= N; ++i)
 	{
-		for (int j = 1; j <= M; ++j)
+		for (int j : { 1, 3, 4 })
 		{
-			if (a[i - 1][j - 1] == '0')
-				continue;
-
-			d[i][j] = 1 + min({ d[i - 1][j], d[i][j - 1], d[i - 1][j - 1] });
-			ans = max(ans, d[i][j]);
+			if (d[i - j] == 1)
+			{
+				d[i] = 0;
+				break;
+			}
+			d[i] = 1;
 		}
 	}
 
-	cout << ans * ans;
+	cout << (d[N] == 0 ? "SK" : "CY");
 }
