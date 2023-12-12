f53625db8e502ec46753dbe6a6daecad1a2b3a8c
hayoonlee <htdobad@gmail.com>
2023-12-12 22:56:34 +0900

    백준/1531

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -2,38 +2,31 @@
 using namespace std;
 
 int N, M;
-int a[1030][1030];
-int d[1030][1030];
+int a[105][105];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N >> M;
-	for (int i = 1; i <= N; ++i)
-		for (int j = 1; j <= N; ++j)
-			cin >> a[i][j];
+	for (int i = 1; i <= 100; ++i)
+		fill(a[i], a[i] + 101, M);
 
-	for (int i = 1; i <= N; ++i)
-	{
-		d[i][1] = a[i][1];
-		for (int j = 2; j <= N; ++j)
-			d[i][j] = a[i][j] + d[i][j - 1];
-	}
-
-	while (M--)
+	while (N--)
 	{
 		int x1, y1, x2, y2;
 		cin >> x1 >> y1 >> x2 >> y2;
 
-		int ans = 0;
-		for (int i = x2; i >= x1; --i)
-		{
-			ans += d[i][y2];
-			if (y1 != 1)
-				ans -= d[i][y1 - 1];
-		}
-
-		cout << ans << '\n';
+		for (int i = x1; i <= x2; ++i)
+			for (int j = y1; j <= y2; ++j)
+				--a[i][j];
 	}
+
+	int ans = 0;
+	for (int i = 1; i <= 100; ++i)
+		for (int j = 1; j <= 100; ++j)
+			if (a[i][j] < 0)
+				++ans;
+
+	cout << ans;
 }
