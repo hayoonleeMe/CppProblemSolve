7b691b4f522da98e8e806d557835a38d00e9a1c8
hayoonlee <htdobad@gmail.com>
2023-12-16 02:08:40 +0900

    백준/9184

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,38 +1,36 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int C1, C2;
-int f[45];
+int d[25][25][25];
 
-int fib(int n) {
-    if (n == 1 or n == 2)
-    {
-        ++C1;
-        return 1;  // 코드1
-    }
-    else
-        return (fib(n - 1) + fib(n - 2));
-}
+int Solve(int a, int b, int c)
+{
+	if (a <= 0 or b <= 0 or c <= 0)
+		return 1;
+	if (a > 20 or b > 20 or c > 20)
+		return Solve(20, 20, 20);
 
-void fibonacci(int n) {
-    f[1] = 1;
-    f[2] = 1;
-    for (int i = 3; i <= n; ++i)
-    {
-        ++C2;
-        f[i] = f[i - 1] + f[i - 2]; // 코드2
-    }
+	int& ret = d[a][b][c];
+	if (ret)
+		return ret;
+
+	if (a < b and b < c)
+		return ret = Solve(a, b, c - 1) + Solve(a, b - 1, c - 1) - Solve(a, b - 1, c);
+	else
+		return ret = Solve(a - 1, b, c) + Solve(a - 1, b - 1, c) + Solve(a - 1, b, c - 1) - Solve(a - 1, b - 1, c - 1);
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-    int n;
-    cin >> n;
+	int a, b, c;
+	while (1)
+	{
+		cin >> a >> b >> c;
+		if (a == -1 and b == -1 and c == -1)
+			return 0;
 
-    fib(n);
-    fibonacci(n);
-
-    cout << C1 << ' ' << C2;
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << Solve(a, b, c) << '\n';
+	}
 }
