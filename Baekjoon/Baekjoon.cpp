ba27860bcdabb3eec2f6b7a4734ef3d5fa8dcfd0
hayoonlee <htdobad@gmail.com>
2023-12-25 14:11:34 +0900

    백준/14442

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -2,31 +2,39 @@
 using namespace std;
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
-int N, M;
+int N, M, K;
 string board[1005];
-int dist[1005][1005];
-// x, y, 벽 부순 횟수
+int dist[1005][1005][15];
 queue<tuple<int, int, int>> q;
-queue<tuple<int, int, int>> wq;
+
+int bfs();
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> M;
+	cin >> N >> M >> K;
 	for (int i = 0; i < N; ++i)
 	{
 		cin >> board[i];
-		fill(dist[i], dist[i] + M, -1);
+		for (int j = 0; j < M; ++j)
+			fill(dist[i][j], dist[i][j] + K + 1, -1);
 	}
 
-	dist[0][0] = 1;
+	cout << bfs();
+}
+
+int bfs()
+{
+	dist[0][0][0] = 1;
 	q.push({ 0, 0, 0 });
 
 	while (!q.empty())
 	{
-		int x, y, cnt;
-		tie(x, y, cnt) = q.front();
+		int x, y, broken;
+		tie(x, y, broken) = q.front();
+		if (x == N - 1 && y == M - 1)
+			return dist[x][y][broken];
 		q.pop();
 
 		for (int dir = 0; dir < 4; ++dir)
@@ -36,46 +44,19 @@ int main()
 
 			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
 				continue;
-			if (dist[nx][ny] != -1)
-				continue;
 
-			if (board[nx][ny] == '0')
+			if (board[nx][ny] == '0' && dist[nx][ny][broken] == -1)
 			{
-				dist[nx][ny] = dist[x][y] + 1;
-				q.push({ nx, ny, cnt });
+				dist[nx][ny][broken] = dist[x][y][broken] + 1;
+				q.push({ nx, ny, broken });
 			}
-			else if (board[nx][ny] == '1')
+			else if (board[nx][ny] == '1' && broken <= K && dist[nx][ny][broken + 1] == -1)
 			{
-				dist[nx][ny] = dist[x][y] + 1;
-				wq.push({ nx, ny, cnt + 1 });
+				dist[nx][ny][broken + 1] = dist[x][y][broken] + 1;
+				q.push({ nx, ny, broken + 1 });
 			}
 		}
 	}
 
-	while (!wq.empty())
-	{
-		int x, y, cnt;
-		tie(x, y, cnt) = wq.front();
-		wq.pop();
-
-		for (int dir = 0; dir < 4; ++dir)
-		{
-			int nx = x + dx[dir];
-			int ny = y + dy[dir];
-
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-				continue;
-
-			if (board[nx][ny] == '0')
-			{
-				if (dist[nx][ny] == -1 || dist[x][y] + 1 < dist[nx][ny])
-				{
-					dist[nx][ny] = dist[x][y] + 1;
-					wq.push({ nx, ny, cnt });
-				}
-			}
-		}
-	}
-
-	cout << dist[N - 1][M - 1];
+	return -1;
 }
