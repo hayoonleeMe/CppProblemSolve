66cfeaec5e42df129da05fafe9f07b93c0ce273e
hayoonlee <htdobad@gmail.com>
2023-12-30 23:28:53 +0900

    백준/25418

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,53 +1,36 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
-const int CYCLE = -1;
-const int NOT_VIS = 0;
-int T, n;
-int a[100001];
-int vis[100001];
+
+int A, K;
+int dist[1000005];
+queue<int> q;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> T;
-	while (T--)
-	{
-		cin >> n;
-		for (int j = 1; j <= n; ++j)
-			cin >> a[j];
-		fill(vis + 1, vis + n + 1, NOT_VIS);
+	cin >> A >> K;
+	fill(dist, dist + 1000005, -1);
 
-		for (int j = 1; j <= n; ++j)
+	dist[A] = 0;
+	q.push(A);
+
+	while (!q.empty())
+	{
+		int cur = q.front();
+		q.pop();
+
+		for (int nx : { cur + 1, cur * 2 })
 		{
-			if (vis[j] != NOT_VIS)
+			if (nx > 1000005)
+				continue;
+			if (dist[nx] != -1)
 				continue;
 
-			int cur = j;
-			while (1)
-			{
-				vis[cur] = j;
-				cur = a[cur];
-				if (vis[cur] == j)
-				{
-					while (vis[cur] != CYCLE)
-					{
-						vis[cur] = CYCLE;
-						cur = a[cur];
-					}
-					break;
-				}
-
-				if (vis[cur] != NOT_VIS)
-					break;
-			}
+			dist[nx] = dist[cur] + 1;
+			q.push(nx);
 		}
-
-		int ans = 0;
-		for (int j = 1; j <= n; ++j)
-			if (vis[j] != CYCLE)
-				++ans;
-
-		cout << ans << '\n';
 	}
+
+	cout << dist[K];
 }
