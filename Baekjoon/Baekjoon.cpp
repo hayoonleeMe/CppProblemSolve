9e57d13929c2ddfc10ef329f7acf7e6f55f11233
hayoonlee <htdobad@gmail.com>
2023-12-31 20:51:02 +0900

    백준/16920

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -4,72 +4,75 @@ using namespace std;
 #define Y second
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
-int N, M;	
+int N, M, P;
+int S[12];
 string board[1005];
 bool vis[1005][1005];
-queue<pair<int, int>> q;
-// min_x min_y max_x max_y
-vector<tuple<int, int, int, int>> v;
+// x y cnt
+queue<tuple<int, int, int>> q[12];
+int ans[12];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> M;
-	for (int i = 0; i < N; ++i)
-		cin >> board[i];
-
+	cin >> N >> M >> P;
+	for (int i = 1; i <= P; ++i)
+		cin >> S[i];
 	for (int i = 0; i < N; ++i)
 	{
+		cin >> board[i];
 		for (int j = 0; j < M; ++j)
 		{
-			if (vis[i][j])
-				continue;
-
-			vis[i][j] = 1;
-			q.push({ i, j });
-
-			int min_x = i, min_y = j;
-			int max_x = i, max_y = j;
-			while (!q.empty())
+			if (board[i][j] != '#' && board[i][j] != '.')
 			{
-				pair<int, int> cur = q.front();
-				q.pop();
-
-				for (int dir = 0; dir < 4; ++dir)
-				{
-					int nx = cur.X + dx[dir];
-					int ny = cur.Y + dy[dir];
-
-					if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-						continue;
-					if (vis[nx][ny] || board[cur.X][cur.Y] != board[nx][ny])
-						continue;
-					
-					vis[nx][ny] = 1;
-					q.push({ nx, ny });
-
-					min_x = min(min_x, nx);
-					min_y = min(min_y, ny);
-					max_x = max(max_x, nx);
-					max_y = max(max_y, ny);
-				}
-			}
-
-			char mark = board[min_x][min_y];
-			for (int i = min_x; i <= max_x; ++i)
-			{
-				for (int j = min_y; j <= max_y; ++j)
-				{
-					if (board[i][j] != mark)
-					{
-						cout << "BaboBabo";
-						return 0;
-					}
-				}
+				vis[i][j] = 1;
+				q[board[i][j] - '0'].push({ i, j, 0 });
+				++ans[board[i][j] - '0'];
 			}
 		}
 	}
 
-	cout << "dd";
+	while (1)
+	{
+		bool quit = true;
+		for (int i = 1; i <= P; ++i)
+		{
+			queue<tuple<int, int, int>> nextQ;
+			while (!q[i].empty())
+			{
+				int x, y, cnt;
+				tie(x, y, cnt) = q[i].front();
+				q[i].pop();
+				if (cnt == S[i])
+				{
+					nextQ.push({ x, y, 0 });
+					continue;
+				}
+
+				for (int dir = 0; dir < 4; ++dir)
+				{
+					int nx = x + dx[dir];
+					int ny = y + dy[dir];
+					int ncnt = cnt + 1;
+
+					if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+						continue;
+					if (vis[nx][ny] || board[nx][ny] == '#')
+						continue;
+
+					vis[nx][ny] = 1;
+					q[i].push({ nx, ny, ncnt });
+					++ans[i];
+					quit = false;
+				}
+			}
+			q[i] = nextQ;
+		}
+		if (quit)
+			break;
+	}
+
+	for (int i = 1; i <= P; ++i)
+		cout << ans[i] << ' ';
 }
