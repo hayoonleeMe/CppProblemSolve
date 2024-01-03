a3a5e159f86a0aa0e52a78ecff89c64c6ce1bb34
hayoonlee <htdobad@gmail.com>
2024-01-03 14:07:05 +0900

    백준/14502

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,48 +1,119 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
-
+#define X first
+#define Y second
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
 int N, M;
-vector<int> g[1005];
-bool vis[1005];
-queue<int> q;
+int board[10][10];
+bool vis[10][10];
+vector<pair<int, int>> virus;
+pair<int, int> newWalls[3];
+
+int bfs()
+{
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < M; ++j)
+			if (board[i][j] == 0)
+				vis[i][j] = 0;
+			else if (board[i][j] == 1)
+				vis[i][j] = 1;
+
+	for (const pair<int, int>& p : newWalls)
+		vis[p.X][p.Y] = 1;
+
+	queue<pair<int, int>> q;
+	for (const pair<int, int>& p : virus)
+	{
+		vis[p.X][p.Y] = 1;
+		q.push({ p.X, p.Y });
+	}
+
+	while (!q.empty())
+	{
+		pair<int, int> cur = q.front();
+		q.pop();
+
+		for (int dir = 0; dir < 4; ++dir)
+		{
+			int nx = cur.X + dx[dir];
+			int ny = cur.Y + dy[dir];
+
+			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+				continue;
+			if (vis[nx][ny])
+				continue;
+
+			vis[nx][ny] = 1;
+			q.push({ nx, ny });
+		}
+	}
+
+	int safe = 0;
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < M; ++j)
+			if (!vis[i][j])
+				++safe;
+	return safe;
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N >> M;
-	while (M--)
+	for (int i = 0; i < N; ++i)
 	{
-		int u, v;
-		cin >> u >> v;
-		g[u].push_back(v);
-		g[v].push_back(u);
+		for (int j = 0; j < M; ++j)
+		{
+			cin >> board[i][j];
+			if (board[i][j] == 2)
+				virus.push_back({ i, j });
+		}
 	}
 
 	int ans = 0;
-	for (int i = 1; i <= N; ++i)
+	int a = 0, b = 1, c = 2;
+	for (; a < N * M - 2; ++a)
 	{
-		if (vis[i])
-			continue;
-
-		vis[i] = 1;
-		q.push(i);
-		++ans;
-
-		while (!q.empty())
+		if (board[a / M][a % M] != 0)
 		{
-			int cur = q.front();
-			q.pop();
-
-			for (const int& n : g[cur])
-			{
-				if (vis[n])
-					continue;
-
-				vis[n] = 1;
-				q.push(n);
-			}
+			++b; ++c;
+			continue;
 		}
+
+		for (; b < N * M - 1; ++b)
+		{
+			if (board[b / M][b % M] != 0)
+			{
+				++c;
+				continue;
+			}
+
+			for (; c < N * M; ++c)
+			{
+				if (board[c / M][c % M] != 0)
+				{
+					continue;
+				}
+
+				/*cout << "a : " << a << " -> " << a / M << " , " << a % M << '\n';
+				cout << "b : " << b << " -> " << b / M << " , " << b % M << '\n';
+				cout << "c : " << c << " -> " << c / M << " , " << c % M << '\n';*/
+
+				newWalls[0] = { a / M, a % M };
+				newWalls[1] = { b / M, b % M };
+				newWalls[2] = { c / M, c % M };
+
+				ans = max(ans, bfs());
+				//cout << "======= ans udpate : " << ans << " ========\n";
+			}
+
+			c = b + 2;
+		}
+
+		b = a + 2;
+		c = a + 3;
 	}
 
 	cout << ans;
