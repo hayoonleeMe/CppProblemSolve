1efb85842e381965808d62fd1e15cf4eff857489
hayoonlee <htdobad@gmail.com>
2024-01-03 16:19:52 +0900

    백준/16236

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -4,30 +4,27 @@ using namespace std;
 #define Y second
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
-int N, M;
-int board[10][10];
-bool vis[10][10];
-vector<pair<int, int>> virus;
-pair<int, int> newWalls[3];
+int N;
+int board[22][22];
+// x, y
+list<pair<int, int>> fishes;
+int dist[22][22];
+// 초기 크기 2
+int ssize = 2;
+// 먹은 수
+int eat = 0;
+// 상어 위치
+int sx, sy;
 
-int bfs()
+void Bfs(int x, int y)
 {
+	// dist 업데이트
 	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < M; ++j)
-			if (board[i][j] == 0)
-				vis[i][j] = 0;
-			else if (board[i][j] == 1)
-				vis[i][j] = 1;
-
-	for (const pair<int, int>& p : newWalls)
-		vis[p.X][p.Y] = 1;
+		fill(dist[i], dist[i] + N, -1);
 
 	queue<pair<int, int>> q;
-	for (const pair<int, int>& p : virus)
-	{
-		vis[p.X][p.Y] = 1;
-		q.push({ p.X, p.Y });
-	}
+	dist[x][y] = 0;
+	q.push({ x, y });
 
 	while (!q.empty())
 	{
@@ -39,81 +36,127 @@ int bfs()
 			int nx = cur.X + dx[dir];
 			int ny = cur.Y + dy[dir];
 
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N)
 				continue;
-			if (vis[nx][ny])
+			if (dist[nx][ny] != -1 || board[nx][ny] > ssize)
 				continue;
 
-			vis[nx][ny] = 1;
+			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
 			q.push({ nx, ny });
 		}
 	}
+}
 
-	int safe = 0;
-	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < M; ++j)
-			if (!vis[i][j])
-				++safe;
-	return safe;
+bool compare(const pair<int, int>& a, const pair<int, int>& b)
+{
+	return board[a.X][a.Y] < board[b.X][b.Y];
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> M;
+	cin >> N;
 	for (int i = 0; i < N; ++i)
 	{
-		for (int j = 0; j < M; ++j)
+		for (int j = 0; j < N; ++j)
 		{
 			cin >> board[i][j];
-			if (board[i][j] == 2)
-				virus.push_back({ i, j });
+			if (board[i][j] == 9)
+			{
+				sx = i;
+				sy = j;
+				board[i][j] = 0;
+			}
+			else if (board[i][j] != 0)
+				fishes.push_back({ i, j });
 		}
 	}
+	// 물고기 크기 오름차순 정렬
+	fishes.sort(compare);
 
 	int ans = 0;
-	int a = 0, b = 1, c = 2;
-	for (; a < N * M - 2; ++a)
+	while (1)
 	{
-		if (board[a / M][a % M] != 0)
-		{
-			++b; ++c;
-			continue;
-		}
+		// dist 업데이트
+		Bfs(sx, sy);
 
-		for (; b < N * M - 1; ++b)
+		// 먹을 물고기 고르기
+		if (fishes.empty())
+			break;
+
+		int fx = -1, fy = -1;
+		int fdist = 0x7f7f7f7f;
+		list<pair<int, int>>::iterator fish;
+		for (list<pair<int, int>>::iterator it = fishes.begin(); it != fishes.end(); it++)
 		{
-			if (board[b / M][b % M] != 0)
-			{
-				++c;
+			// 현재 상어보다 큰 물고기 못먹음
+			if (board[it->X][it->Y] >= ssize)
+				break;
+
+			// 도달 불가
+			if (dist[it->X][it->Y] == -1)
 				continue;
-			}
 
-			for (; c < N * M; ++c)
+			// 거리가 가장 가까운 놈
+			if (fdist > dist[it->X][it->Y])
 			{
-				if (board[c / M][c % M] != 0)
-				{
-					continue;
-				}
-
-				/*cout << "a : " << a << " -> " << a / M << " , " << a % M << '\n';
-				cout << "b : " << b << " -> " << b / M << " , " << b % M << '\n';
-				cout << "c : " << c << " -> " << c / M << " , " << c % M << '\n';*/
-
-				newWalls[0] = { a / M, a % M };
-				newWalls[1] = { b / M, b % M };
-				newWalls[2] = { c / M, c % M };
-
-				ans = max(ans, bfs());
-				//cout << "======= ans udpate : " << ans << " ========\n";
+				fdist = dist[it->X][it->Y];
+				fx = it->X; 
+				fy = it->Y;
+				fish = it;
 			}
-
-			c = b + 2;
+			else if (fdist == dist[it->X][it->Y])
+			{
+				// 거리가 같으면 위에 있는 놈
+				if (it->X < fx)
+				{
+					fx = it->X;
+					fy = it->Y;
+					fish = it;
+				}
+				// 같은 높이면 왼쪽인 놈
+				else if (fx == it->X)
+				{
+					if (it->Y < fy)
+					{
+						fx = it->X;
+						fy = it->Y;
+						fish = it;
+					}
+				}
+			}
 		}
+		// 먹을 수 있는 물고기 X
+		if (fx == -1)
+			break;
 
-		b = a + 2;
-		c = a + 3;
+		// 먹음
+		fishes.erase(fish);
+		board[fx][fy] = 0;
+		++eat;
+		if (eat == ssize)
+		{
+			eat = 0;
+			++ssize;
+		}
+		ans += dist[fx][fy];
+		// 상어 위치 업데이트
+		sx = fx;
+		sy = fy;
+
+
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+				if (dist[i][j] == -1)
+					cout << "8 ";
+				else
+					cout << dist[i][j] << ' ';
+			cout << '\n';
+		}
+		cout << "fish " << fx << ", " << fy << '\n';
+		cout << "ans : " << ans << "\n\n";
 	}
 
 	cout << ans;
