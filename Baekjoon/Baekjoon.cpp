6f13207f2b38678798b60fd2e037d5cee9362177
hayoonlee <htdobad@gmail.com>
2024-01-05 11:28:54 +0900

    백준/4963

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -2,78 +2,64 @@
 using namespace std;
 #define X first
 #define Y second
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-int N, M;
-int board[105][105];
-int vis[105][105];
+int dx[8] = { 1, 1, 0, -1, -1, -1, 0, 1 };
+int dy[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+int W, H;
+int board[52][52];
+bool vis[52][52];
 queue<pair<int, int>> q;
-int ans;
-
-void Bfs()
-{
-	while (!q.empty())
-	{
-		pair<int, int> cur = q.front();
-		q.pop();
-
-		for (int dir = 0; dir < 4; ++dir)
-		{
-			int nx = cur.X + dx[dir];
-			int ny = cur.Y + dy[dir];
-
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-				continue;
-
-			// 치즈면 공기에 접촉하는 횟수 +1
-			if (board[nx][ny] == 1)
-			{
-				++vis[nx][ny];
-			}
-			else if (!vis[nx][ny])
-			{
-				vis[nx][ny] = 1;
-				q.push({ nx, ny });
-			}
-		}
-	}
-}
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> M;
-	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < M; ++j)
-			cin >> board[i][j];
-
-	// 초기 vis 업데이트
-	vis[0][0] = 1;
-	q.push({ 0, 0 });
-	Bfs();
-
 	while (1)
 	{
-		for (int i = 0; i < N; ++i)
+		cin >> W >> H;
+		if (W == 0 && H == 0)
+			break;
+
+		for (int i = 0; i < H; ++i)
 		{
-			for (int j = 0; j < M; ++j)
+			fill(vis[i], vis[i] + W, 0);
+			for (int j = 0; j < W; ++j)
+				cin >> board[i][j];
+		}
+			
+		int ans = 0;
+		for (int i = 0; i < H; ++i)
+		{
+			for (int j = 0; j < W; ++j)
 			{
-				if (vis[i][j] >= 2)
+				if (vis[i][j] || !board[i][j])
+					continue;
+
+				vis[i][j] = 1;
+				q.push({ i, j });
+				++ans;
+
+				while (!q.empty())
 				{
-					board[i][j] = 0;
-					vis[i][j] = 1;
-					q.push({ i, j });
+					pair<int, int> cur = q.front();
+					q.pop();
+
+					for (int dir = 0; dir < 8; ++dir)
+					{
+						int nx = cur.X + dx[dir];
+						int ny = cur.Y + dy[dir];
+
+						if (nx < 0 || nx >= H || ny < 0 || ny >= W)
+							continue;
+						if (vis[nx][ny] || !board[nx][ny])
+							continue;
+
+						vis[nx][ny] = 1;
+						q.push({ nx, ny });
+					}
 				}
 			}
 		}
 
-		if (q.size() == 0)
-			break;
-
-		++ans;
-		Bfs();
+		cout << ans << '\n';
 	}
-
-	cout << ans;
 }
