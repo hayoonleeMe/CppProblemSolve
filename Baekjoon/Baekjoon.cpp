6825d2b8a326e7f23b79482e2709f5d3ffaaece3
hayoonlee <htdobad@gmail.com>
2024-01-06 12:29:49 +0900

    백준/2589

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -4,83 +4,54 @@ using namespace std;
 #define Y second
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
-int N, L, R;
-int board[52][52];
-bool vis[52][52];
+int N, M;
+string board[52];
+int dist[52][52];
+vector<pair<int, int>> v;
+queue<pair<int, int>> q;
 int ans;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> L >> R;
+	cin >> N >> M;
 	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < N; ++j)
-			cin >> board[i][j];
-
-	while (1)
 	{
-		bool transfer = false;
+		cin >> board[i];
+		for (int j = 0; j < M; ++j)
+			if (board[i][j] == 'L')
+				v.push_back({ i, j });
+	}
+
+	for (const pair<int, int>& start : v)
+	{
 		for (int i = 0; i < N; ++i)
+			fill(dist[i], dist[i] + M, -1);
+
+		dist[start.X][start.Y] = 0;
+		q.push({ start.X, start.Y });
+
+		while (!q.empty())
 		{
-			for (int j = 0; j < N; ++j)
+			pair<int, int> cur = q.front();
+			q.pop();
+			ans = max(ans, dist[cur.X][cur.Y]);
+
+			for (int dir = 0; dir < 4; ++dir)
 			{
-				if (vis[i][j])
+				int nx = cur.X + dx[dir];
+				int ny = cur.Y + dy[dir];
+
+				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+					continue;
+				if (dist[nx][ny] != -1 || board[nx][ny] != 'L')
 					continue;
 
-				// 연합 생성
-				vector<pair<int, int>> v;
-
-				vis[i][j] = ans + 1;
-				v.push_back({ i, j });
-				int sum = board[i][j], cnt = 1;
-				int idx = 0;
-				while (idx < v.size())
-				{
-					pair<int, int> cur = v[idx++];
-
-					for (int dir = 0; dir < 4; ++dir)
-					{
-						int nx = cur.X + dx[dir];
-						int ny = cur.Y + dy[dir];
-
-						if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-							continue;
-
-						if (vis[nx][ny])
-							continue;
-
-						int gap = abs(board[cur.X][cur.Y] - board[nx][ny]);
-						if (gap >= L && gap <= R)
-						{
-							sum += board[nx][ny];
-							++cnt;
-
-							vis[nx][ny] = ans + 1;
-							v.push_back({ nx, ny });
-						}
-					}
-				}
-
-				// 인구 이동
-				if (cnt > 1)
-				{
-					int uni = sum / cnt;
-					for (const pair<int, int>& p : v)
-						board[p.X][p.Y] = uni;
-					transfer = true;
-				}
+				dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+				q.push({ nx, ny });
 			}
 		}
-
-		if (transfer)
-		{
-			++ans;
-			for (int i = 0; i < N; ++i)
-				fill(vis[i], vis[i] + N, 0);
-		}
-		else
-			break;
 	}
 
 	cout << ans;
