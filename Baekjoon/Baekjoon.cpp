2996ee98eda27e0fac77750ee48b4226c0f30990
hayoonlee <htdobad@gmail.com>
2024-01-08 14:26:26 +0900

    백준/11048

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,36 +1,35 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int a[1002];
-int d[1002];
+int N, M;
+int a[1002][1002];
+int d[1002][1002];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
+	cin >> N >> M;
+	for (int i = 1; i <= N; ++i)
+		for (int j = 1; j <= M; ++j)
+			cin >> a[i][j];
+	d[1][1] = a[1][1];
+
 	for (int i = 1; i <= N; ++i)
 	{
-		cin >> a[i];
-		d[i] = 1;
+		for (int j = 1; j <= M; ++j)
+		{
+			if (i - 1 > 0)
+				d[i][j] = max(d[i][j], d[i - 1][j] + a[i][j]);
+
+			if (j - 1 > 0)
+				d[i][j] = max(d[i][j], d[i][j - 1] + a[i][j]);
+
+			if (i - 1 > 0 && j - 1 > 0)
+				d[i][j] = max(d[i][j], d[i - 1][j - 1] + a[i][j]);
+		}
 	}
 
-	for (int i = 2; i <= N; ++i)
-	{ 
-		for (int j = 1; j < i; ++j)
-			if (a[i] < a[j])
-				d[i] = max(d[i], d[j] + 1);
-	}
-
-	cout << *max_element(d + 1, d + N + 1);
+	cout << d[N][M];
 }
 
-//10 30 10 20 20 10
-//d[i] : i번째 수를포함하는 가장 긴 감소하는 부분 수열의 길이
-//d[1] = 1 => 10
-//d[2] = 1 => 30
-//d[3] = 2 => 30 10
-//d[4] = 2 => 30 20
-//d[5] = 2 => 30 20
-//d[6] = 3 => 30 20 10 => max(1, d[5] + 1, d[4] + 1, d[2] + 1)
