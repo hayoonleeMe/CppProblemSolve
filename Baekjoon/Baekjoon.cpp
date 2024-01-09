b1952e96868498b4dc474594716df7cca45638ec
hayoonlee <htdobad@gmail.com>
2024-01-10 00:26:47 +0900

    백준/1495

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,47 +1,62 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int a[102][102];
-long long d[102][102];
+int N, S, M;
+int a[52];
+bool d[52][1002];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-	for (int i = 1; i <= N; ++i)
-		for (int j = 1; j <= N; ++j)
-			cin >> a[i][j];
+	cin >> N >> S >> M;
+	for (int i = 0; i < N; ++i)
+		cin >> a[i];
 
-	d[1][1] = 1;
-	for (int i = 1; i <= N; ++i)
+	if (S - a[0] >= 0)
+		d[0][S - a[0]] = 1;
+	if (S + a[0] <= M)
+		d[0][S + a[0]] = 1;
+
+	for (int i = 1; i < N; ++i)
 	{
-		for (int j = 1; j <= N; ++j)
+		for (int j = 0; j <= M; ++j)
 		{
-			// x
-			for (int ii = 1; ii < i; ++ii)
-				if (ii + a[ii][j] == i)
-					d[i][j] += d[ii][j];
-			// y
-			for (int jj = 1; jj < j; ++jj)
-				if (jj + a[i][jj] == j)
-					d[i][j] += d[i][jj];
+			if (d[i - 1][j])
+			{
+				if (j - a[i] >= 0)
+					d[i][j - a[i]] = 1;
+				if (j + a[i] <= M)
+					d[i][j + a[i]] = 1;
+			}
 		}
 	}
 
-	cout << d[N][N];
+	for (int i = M; i >= 0; --i)
+	{
+		if (d[N - 1][i])
+		{
+			cout << i;
+			return 0;
+		}
+	}
+
+	cout << -1;
 }
 
-//4
-//2 3 3 1
-//1 2 1 3
-//1 2 3 1
-//3 1 1 0
+//14 40 243
+//74 39 127 95 63 140 99 96 154 18 137 162 14 88
 //
-//1 0 1 0
-//0 0 0 0
-//1 1 0 1
-//1 0 1 3
+//d[1][114] = 1
 //
-//d[i][j] : i행j열 칸까지 올 수 있는 경로의 개수
+//d[2][75] = 1
+//d[2][153] = 1
+//
+//3 5 10
+//5 3 7
+//
+//d[1][0] = 1
+//d[1][10] = 1
+//
+//d[2][3] = 1
+//d[2][7] = 1
