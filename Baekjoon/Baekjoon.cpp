1474abf2ef56ab02cea4170ec1e0f7da23c054cc
hayoonlee <htdobad@gmail.com>
2024-01-09 17:56:29 +0900

    백준/11060

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,35 +1,38 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, M;
-int a[1002][1002];
-int d[1002][1002];
+int N;
+int a[1005];
+int d[1005];
+const int MAX = 0x7f7f7f7f;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> M;
+	cin >> N;
 	for (int i = 1; i <= N; ++i)
-		for (int j = 1; j <= M; ++j)
-			cin >> a[i][j];
-	d[1][1] = a[1][1];
+		cin >> a[i];
+	fill(d, d + N + 1, MAX);
+	d[1] = 0;
 
-	for (int i = 1; i <= N; ++i)
+	for (int i = 2; i <= N; ++i)
 	{
-		for (int j = 1; j <= M; ++j)
-		{
-			if (i - 1 > 0)
-				d[i][j] = max(d[i][j], d[i - 1][j] + a[i][j]);
-
-			if (j - 1 > 0)
-				d[i][j] = max(d[i][j], d[i][j - 1] + a[i][j]);
-
-			if (i - 1 > 0 && j - 1 > 0)
-				d[i][j] = max(d[i][j], d[i - 1][j - 1] + a[i][j]);
-		}
+		for (int j = 1; j < i; ++j)
+			if (j + a[j] >= i)
+				d[i] = min(d[i], d[j] + 1);
 	}
 
-	cout << d[N][M];
+	if (d[N] == MAX)
+		cout << -1;
+	else
+		cout << d[N];
 }
 
+//10
+//1 2 0 1 3 2 1 5 4 2
+//d[1] = 0
+//d[2] = 1 => 1+1
+//d[3] = 2
+//d[4] = 
+//d[i] = min(d[1]+1, d[2]+1, ..., d[i-1]+1) (단, i번째에 도달할 수 있을 때만)
