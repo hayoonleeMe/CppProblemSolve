1f45f778fa41383dc887a835bdbdfc2c5eae375b
hayoonlee <htdobad@gmail.com>
2024-01-14 00:23:15 +0900

    백준/28136

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 
 int N;
-long long d[82];
+int a[1'000'002];
+int ans;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N;
-	d[1] = d[2] = 1;
+	for (int i = 1; i <= N; ++i)
+		cin >> a[i];
 
-	for (int i = 3; i <= 81; ++i)
-		d[i] = d[i - 1] + d[i - 2];
+	a[0] = a[N];
+	for (int i = 0; i < N; ++i)
+	{
+		if (a[i] >= a[i + 1])
+			++ans;
+	}
 
-	long long ans = 2 * d[N + 1] + 2 * d[N];
 	cout << ans;
 }
