fee4b1478417a31d7b71e39cefe459a46fa87446
hayoonlee <htdobad@gmail.com>
2024-01-15 14:15:18 +0900

    백준/13258

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,61 +1,31 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-double C;
-int coin;
-int N;
-int a[100'002];
-// i=N, j=스트릭 사용 횟수
-int d[100'002][3];
+int N, j, c;
+double d[1002];
+int sum[1002];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> C;
-	coin = C / 0.99;
-
 	cin >> N;
-	for (int i = 1; i <= N; ++i)
-		cin >> a[i];
+	cin >> d[0];
+	sum[0] += d[0];
 
-	if (a[1] == 0)
-		d[1][1] = 1;
-	else
-		d[1][0] = 1;
-
-	for (int i = 2; i <= N; ++i)
+	for (int i = 1; i < N; ++i)
 	{
-		if (a[i] == 0)
-		{
-			d[i][0] = 0;
-			d[i][1] = d[i - 1][0] + 1;
-			d[i][2] = d[i - 1][1] + 1;
-		}
-		else
-		{
-			d[i][0] = d[i - 1][0] + 1;
-			d[i][1] = d[i - 1][1] + 1;
-			d[i][2] = d[i - 1][2] + 1;
-		}
+		int t; cin >> t;
+		sum[0] += t;
 	}
 
-	int ans = 0;
-	for (int i = 1; i <= N; ++i)
-		for (int j = 0; j <= coin; ++j)
-			ans = max(ans, d[i][j]);
+	cin >> j >> c;
 
-	cout << ans << '\n' << *max_element(a, a + N + 1);
+	for (int i = 1; i <= c; ++i)
+	{
+		d[i] = d[i - 1] + j * (d[i - 1] / sum[i - 1]);
+		sum[i] = sum[i - 1] + j;
+	}
+
+	cout << fixed << setprecision(9) << d[c];
 }
-
-//d[i][j] : i번째 날까지 스트릭을 j번 사용했을 때 풀 수 있는 최대 문제 수
-//
-//0이면
-//d[i][0] = 0
-//d[i][1] = d[i-1][0] + 1
-//d[i][2] = d[i-1][1] + 1
-//
-//0이 아니면
-//d[i][0] = d[i-1][0] + 1
-//d[i][1] = d[i-1][1] + 1
-//d[i][2] = d[i-1][2] + 1
