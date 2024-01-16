c298cb6885fdf15910b9cf47f635885db8b9105a
hayoonlee <htdobad@gmail.com>
2024-01-16 16:21:12 +0900

    백준/9252

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,45 +1,73 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int board[20][20];
-// i행 j열에 도달하는 경우의수 0:가로 1:세로 2:대각
-int d[20][20][3];
+string A, B;
+int d[1002][1002];
+
+void LCS(int i, int j)
+{
+	if (i <= 0 || j <= 0)
+		return;
+
+	if (A[i - 1] == B[j - 1])
+	{
+		LCS(i - 1, j - 1);
+		cout << A[i - 1];
+	}
+	else
+	{
+		if (d[i - 1][j] > d[i][j - 1])
+			LCS(i - 1, j);
+		else
+			LCS(i, j - 1);
+	}
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-	for (int i = 1; i <= N; ++i)
-		for (int j = 1; j <= N; ++j)
-			cin >> board[i][j];
-	
-	d[1][2][0] = 1;
-	for (int i = 1; i <= N; ++i)
+	cin >> A >> B;
+	for (int i = 1; i <= A.size(); ++i)
 	{
-		for (int j = 3; j <= N; ++j)
+		for (int j = 1; j <= B.size(); ++j)
 		{
-			// 벽 긁으면 안됨
-			if (board[i][j] != 1)
-			{
-				d[i][j][0] = d[i][j - 1][0] + d[i][j - 1][2];
-				d[i][j][1] = d[i - 1][j][1] + d[i - 1][j][2];
-
-				if (board[i-1][j] != 1 && board[i][j-1] != 1)
-					d[i][j][2] = d[i - 1][j - 1][0] + d[i - 1][j - 1][1] + d[i - 1][j - 1][2];
-			}
+			if (A[i - 1] == B[j - 1])
+				d[i][j] = d[i - 1][j - 1] + 1;
+			else
+				d[i][j] = max(d[i - 1][j], d[i][j - 1]);
 		}
 	}
 
-	cout << d[N][N][0] + d[N][N][1] + d[N][N][2];
+	cout << d[A.size()][B.size()] << '\n';
+
+	if (d[A.size()][B.size()] > 0)
+		LCS(A.size(), B.size());
 }
 
-//i행 j열 빈칸을 가로, 세로, 대각 파이프의 끝으로 도달하는 경우의수
-//d[i][j][0 or 1 or 2] 0:가로 1:세로 2:대각
+//ACAYKP
+//CAPCAK
+//ACAK
 //
-//d[i][j][0] = d[i][j-1][0] + d[i][j-1][2]
+//d[i][j] : 첫 문자열의 i번째까지 부분수열과 두번째 문자열의 j번째까지 부분수열에서 LCS 길이
+//d[i][j] = (첫 문자열의 i번째 문자 == 두번째 문자열의 j번째 문자) d[i-1][j-1] + 1
+//		  (다르면) max(d[i-1][j], d[i][j-1])
 //
-//d[i][j][1] = d[i-1][j][1] + d[i-1][j][2]
+//d[1][1] = 0
 //
-//d[i][j][2] = d[i-1][j-1][0] + d[i-1][j-1][1]
+//d[1][2] = d[0][1] + 1 = 1
+//d[1][3] = max(d[1][2], d[0][3]) = 1
+//d[1][4] = 1
+//d[1][5] = 1
+//d[1][6] = 1
+//
+//d[2][1] = d[1][0] + 1 = 1
+//d[2][2] = max(d[2][1], d[1][2]) =1
+//d[2][3] = 1
+//d[2][4] = d[1][3] + 1 = 2
+//d[2][5] = 2
+//d[2][6] = 2
+//
+//d[3][1] = 1
+//d[3][2] = d[2][1] + 1 = 2
+//...
