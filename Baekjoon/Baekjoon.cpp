09c183eb59c693933c4f213f7c82ee7495a82fe7
hayoonlee <htdobad@gmail.com>
2024-01-20 17:08:30 +0900

    백준/5557

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,73 +1,31 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-string A, B;
-int d[1002][1002];
-
-void LCS(int i, int j)
-{
-	if (i <= 0 || j <= 0)
-		return;
-
-	if (A[i - 1] == B[j - 1])
-	{
-		LCS(i - 1, j - 1);
-		cout << A[i - 1];
-	}
-	else
-	{
-		if (d[i - 1][j] > d[i][j - 1])
-			LCS(i - 1, j);
-		else
-			LCS(i, j - 1);
-	}
-}
+int N;
+int a[102];
+long long d[102][21];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> A >> B;
-	for (int i = 1; i <= A.size(); ++i)
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+		cin >> a[i];
+
+	d[0][a[0]] = 1;
+	for (int i = 1; i < N - 1; ++i)
 	{
-		for (int j = 1; j <= B.size(); ++j)
+		for (int j = 0; j <= 20; ++j)
 		{
-			if (A[i - 1] == B[j - 1])
-				d[i][j] = d[i - 1][j - 1] + 1;
-			else
-				d[i][j] = max(d[i - 1][j], d[i][j - 1]);
+			if (d[i - 1][j] > 0)
+			{
+				if (j + a[i] <= 20)
+					d[i][j + a[i]] += d[i-1][j];
+				if (j - a[i] >= 0)
+					d[i][j - a[i]] += d[i-1][j];
+			}
 		}
 	}
-
-	cout << d[A.size()][B.size()] << '\n';
-
-	if (d[A.size()][B.size()] > 0)
-		LCS(A.size(), B.size());
+	cout << d[N - 2][a[N - 1]];
 }
-
-//ACAYKP
-//CAPCAK
-//ACAK
-//
-//d[i][j] : 첫 문자열의 i번째까지 부분수열과 두번째 문자열의 j번째까지 부분수열에서 LCS 길이
-//d[i][j] = (첫 문자열의 i번째 문자 == 두번째 문자열의 j번째 문자) d[i-1][j-1] + 1
-//		  (다르면) max(d[i-1][j], d[i][j-1])
-//
-//d[1][1] = 0
-//
-//d[1][2] = d[0][1] + 1 = 1
-//d[1][3] = max(d[1][2], d[0][3]) = 1
-//d[1][4] = 1
-//d[1][5] = 1
-//d[1][6] = 1
-//
-//d[2][1] = d[1][0] + 1 = 1
-//d[2][2] = max(d[2][1], d[1][2]) =1
-//d[2][3] = 1
-//d[2][4] = d[1][3] + 1 = 2
-//d[2][5] = 2
-//d[2][6] = 2
-//
-//d[3][1] = 1
-//d[3][2] = d[2][1] + 1 = 2
-//...
