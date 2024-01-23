ca1c61a664a11e97fde7c43e2a1668fb678ac033
hayoonlee <htdobad@gmail.com>
2024-01-24 00:33:48 +0900

    백준/10816

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,33 +1,22 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, M, a, b;
-int day[202];
-int page[202];
-int dp[22][202];
+int N, M, target;
+int a[500'002];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> M;
-	for (int i = 1; i <= M; ++i)
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+		cin >> a[i];
+	sort(a, a + N);
+
+	cin >> M;
+	while (M--)
 	{
-		cin >> a >> b;
-		day[i] = a;
-		page[i] = b;
+		cin >> target;
+		cout << upper_bound(a, a + N, target) - lower_bound(a, a + N, target) << ' ';
 	}
-
-	for (int d = 1; d <= N; ++d)
-	{
-		for (int c = 1; c <= M; ++c)
-		{
-			dp[c][d] = dp[c - 1][d];
-
-			if (d >= day[c])
-				dp[c][d] = max(dp[c][d], page[c] + dp[c-1][d-day[c]]);
-		}
-	}
-
-	cout << dp[M][N];
 }
