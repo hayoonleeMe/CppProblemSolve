18bafff05e6f205db1c7b4b60652fb68000ddba7
hayoonlee <htdobad@gmail.com>
2024-01-25 01:17:30 +0900

    백준/18870

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int N;
 int a[1'000'003];
-int s[1'000'003];
-vector<int> v;
+vector<int> uni;
 
 int main()
 {
@@ -14,16 +13,14 @@ int main()
 	for (int i = 0; i < N; ++i)
 	{
 		cin >> a[i];
-		s[i] = a[i];
+		uni.push_back(a[i]);
 	}
-	sort(s, s + N);
+	sort(uni.begin(), uni.end());
+
+	// unique() : 중복되지 않은 값들을 앞으로 몰아주고 나머지는 쓰레기값 들어감, 쓰레기값이 시작되는 구간을 반환
+	// 따라서 모든 쓰레기값을 erase하고 벡터에는 중복되지 않은 값만 남는다.
+	uni.erase(unique(uni.begin(), uni.end()), uni.end());
 
 	for (int i = 0; i < N; ++i)
-	{
-		if (s[i] != s[i + 1])
-			v.push_back(s[i]);
-	}
-
-	for (int i = 0; i < N; ++i)
-		cout << lower_bound(v.begin(), v.end(), a[i]) - v.begin() << ' ';
+		cout << lower_bound(uni.begin(), uni.end(), a[i]) - uni.begin() << ' ';
 }
