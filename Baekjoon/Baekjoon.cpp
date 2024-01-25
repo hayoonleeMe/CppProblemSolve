5f0686ac35ed96d77878faf3ad75dbcc68edd312
hayoonlee <htdobad@gmail.com>
2024-01-25 15:21:24 +0900

    백준/1654

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,33 +1,39 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int U[1002];
-vector<int> two;
+int K, N;
+int a[10'002];
 
+bool Solve(long long x)
+{
+	long long num = 0;
+	for (int i = 0; i < K; ++i)
+		num += a[i] / x;
+	return num >= N;
+}
+
+// parametric search
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-	for (int i = 0; i < N; ++i)
-		cin >> U[i];
-	sort(U, U + N);
-	
-	for (int x = 0; x < N; ++x)
-		for (int y = x; y < N; ++y)
-			two.push_back(U[x] + U[y]);
-	sort(two.begin(), two.end());	
+	cin >> K >> N;
+	for (int i = 0; i < K; ++i)
+		cin >> a[i];
 
-	for (int k = N - 1; k > 0; --k)
+	long long st = 1;
+	long long en = 0x7fffffff;
+
+	while (st < en)
 	{
-		for (int z = 0; z < k; ++z)
-		{
-			if (binary_search(two.begin(), two.end(), U[k] - U[z]))
-			{
-				cout << U[k];
-				return 0;
-			}
-		}
+		// st와 en이 1차이 날 때, 무한루프에 빠짐
+		// st=1, en=2, mid = 1, Solve(mid) = true => st =1 => 무한루프
+		//long long mid = (st + en) / 2;
+		long long mid = (st + en + 1) / 2;
+		if (Solve(mid))
+			st = mid;
+		else
+			en = mid - 1;
 	}
+	cout << st;
 }
