47a165f41fafb7f657963f2cc4fcb26cd9fa1957
hayoonlee <htdobad@gmail.com>
2024-01-27 02:23:17 +0900

    백준/17245

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,33 +1,46 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int nA, nB;
-int A[500'002];
-int B[500'002];
-vector<int> ans;
+int N;
+int a[1002][1002];
+long long total;
+
+bool Solve(int x)
+{
+	double sum = 0;
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < N; ++j)
+			sum += min(x, a[i][j]);
+
+	return (sum / total) >= 0.5;
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> nA >> nB;
-	for (int i = 0; i < nA; ++i)
-		cin >> A[i];
-	sort(A, A + nA);
-	for (int i = 0; i < nB; ++i)
-		cin >> B[i];
-	sort(B, B + nB);
-
-	for (int i = 0; i < nA; ++i)
-		if (!binary_search(B, B + nB, A[i]))
-			ans.push_back(A[i]);
-
-	if (ans.size() > 0)
+	cin >> N;
+	for (int i = 0; i < N; ++i)
 	{
-		cout << ans.size() << '\n';
-		for (int i = 0; i < ans.size(); ++i)
-			cout << ans[i] << ' ';
+		for (int j = 0; j < N; ++j)
+		{
+			cin >> a[i][j];
+			total += a[i][j];
+		}
 	}
+		
+	int st = 0;
+	int en = 10'000'000;
+	while (st < en)
+	{
+		int mid = (st + en) / 2;
+		if (Solve(mid))
+			en = mid;
+		else
+			st = mid + 1;
+	}
+	if (Solve(st))
+		cout << st;
 	else
 		cout << 0;
 }
