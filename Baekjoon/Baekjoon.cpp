a3af145b5429167a54ef57cbdff0e552f43889e4
hayoonlee <htdobad@gmail.com>
2024-01-27 23:12:54 +0900

    백준/3541

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,36 +1,35 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-long long X, Y;
-int Z;
+int N, M, u, d;
 
-bool Solve(long long mid)
+bool Solve(int mid)
 {
-	int gap = mid - X;
-	int t = (Y + gap) * 100 / mid;
-	return t > Z;
+	return mid * u + (N - mid) * -d > 0;
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> X >> Y;
-	Z = Y * 100 / X;
+	cin >> N >> M;
 
-	long long st = X + 1;
-	long long en = 2 * X;
-	while (st < en)
+	int ans = 0x7fffffff;
+	for (int i = 0; i < M; ++i)
 	{
-		long long mid = (st + en) / 2;
-		
-		if (Solve(mid))
-			en = mid;
-		else
-			st = mid + 1;
+		cin >> u >> d;
+
+		int st = 1;
+		int en = N;
+		while (st < en)
+		{
+			int mid = (st + en) / 2;
+			if (Solve(mid))
+				en = mid;
+			else
+				st = mid + 1;
+		}
+		ans = min(ans, st * u + (N - st) * -d);
 	}
-	if (Solve(st))
-		cout << st - X;
-	else
-		cout << -1;
+	cout << ans;
 }
