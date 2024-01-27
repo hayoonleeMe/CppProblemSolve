7ebfec5962bd28c45e2401b17f2252e56a7378f0
hayoonlee <htdobad@gmail.com>
2024-01-27 10:26:30 +0900

    백준/2121

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,38 +1,29 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, M;
-int a[1'000'002];
-
-bool Solve(long long x)
-{
-	long long sum = 0;
-	for (int i = 0; i < N; ++i)
-		sum += x / a[i];
-	return sum >= M;
-}
+int N, A, B, a, b;
+pair<int, int> arr[500'002];
+int ans;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> M;
+	cin >> N >> A >> B;
 	for (int i = 0; i < N; ++i)
-		cin >> a[i];
-
-	long long st = 1;
-	long long en = 1'000'000'000'000;
-	long long ans;
-	while (st < en)
 	{
-		long long mid = (st + en) / 2;
-		if (Solve(mid))
-		{
-			en = mid;
-			ans = mid;
-		}
-		else
-			st = mid + 1;
+		cin >> a >> b;
+		arr[i] = { a,b };
+	}
+	sort(arr, arr + N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		int oA = arr[i].first;
+		int oB = arr[i].second;
+
+		if (binary_search(arr, arr + N, make_pair(oA + A, oB )) && binary_search(arr, arr + N, make_pair(oA, oB + B)) && binary_search(arr, arr + N, make_pair(oA + A, oB + B)))
+			++ans;
 	}
 	cout << ans;
 }
