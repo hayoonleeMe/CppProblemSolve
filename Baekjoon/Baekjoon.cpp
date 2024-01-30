98b8284c5ebeee5ea3059faef815b0da92611bb6
hayoonlee <htdobad@gmail.com>
2024-01-31 03:54:27 +0900

    백준/2512

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,28 +1,30 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int S, C;
-int L[1'000'002];
+int N;
+int a[10'002];
+long long total;
 
 bool Solve(long long mid)
 {
-	long long cnt = 0;
-	for (int i = 0; i < S; ++i)
-		cnt += L[i] / mid;
-	return cnt >= C;
+	long long sum = 0;
+	for (int i = 0; i < N; ++i)
+		sum += min((long long)a[i], mid);
+	return sum <= total;
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> S >> C;
-	for (int i = 0; i < S; ++i)
-		cin >> L[i];
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+		cin >> a[i];
+	cin >> total;
 
 	long long st = 1;
-	long long en = 1'000'000'000;
-	while (st < en)
+	long long en = *max_element(a, a + N);
+	while (st < en)	
 	{
 		long long mid = (st + en + 1) / 2;
 		if (Solve(mid))
@@ -30,9 +32,5 @@ int main()
 		else
 			en = mid - 1;
 	}
-
-	long long remain = -st * C;
-	for (int i = 0; i < S; ++i)
-		remain += L[i];
-	cout << remain;
+	cout << st;
 }
