9476e27a0924a8253396fb903ce1ddb9de189cee
hayoonlee <htdobad@gmail.com>
2024-02-17 12:12:22 +0900

    백준/15683

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,9 +1,100 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { 1, 0, -1, 0 };
+
 int N, M;
-int a[200'002];
-long long D[200'002];
+int board[10][10];
+// x, y, num
+vector<tuple<int, int, int>> cctvs;
+// x, y, num, dir
+vector<tuple<int, int, int, int>> chosen(70'000);
+
+int dup[10][10];
+int ans = 0x7fffffff;
+
+void mark(int x, int y, int d)
+{
+	pair<int, int> cur = { x, y };
+	while (1)
+	{
+		int nx = cur.first + dx[d];
+		int ny = cur.second + dy[d];
+
+		if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+			break;
+		if (dup[nx][ny] == 6)
+			break;
+
+		dup[nx][ny] = 7;
+		cur.first = nx;
+		cur.second = ny;
+	}
+}
+
+void func(int k)
+{
+	if (k == cctvs.size())
+	{
+		for (int i = 0; i < N; ++i)
+			for (int j = 0; j < M; ++j)
+				dup[i][j] = board[i][j];
+
+		for (int i = 0; i < cctvs.size(); ++i)
+		{
+			int x, y, num, dir;
+			tie(x, y, num, dir) = chosen[i];
+
+			if (num == 1)
+			{
+				int dirs[] = { dir };
+				for (int d : dirs)
+					mark(x, y, d);
+			}
+			else if (num == 2)
+			{
+				int dirs[] = { dir, (dir + 2) % 4 };
+				for (int d : dirs)
+					mark(x, y, d);
+			}
+			else if (num == 3)
+			{
+				int dirs[] = { dir, (dir + 3) % 4 };
+				for (int d : dirs)
+					mark(x, y, d);
+			}
+			else if (num == 4)
+			{
+				int dirs[] = { dir, (dir + 3) % 4, (dir + 2) % 4 };
+				for (int d : dirs)
+					mark(x, y, d);
+			}
+			else if (num == 5)
+			{
+				int dirs[] = { 0, 1, 2, 3 };
+				for (int d : dirs)
+					mark(x, y, d);
+			}
+		}
+
+		int cnt = 0;
+		for (int i = 0; i < N; ++i)
+			for (int j = 0; j < M; ++j)
+				if (dup[i][j] == 0)
+					++cnt;
+		ans = min(ans, cnt);
+		return;
+	}
+
+	for (int i = 0; i < 4; ++i)
+	{
+		int x, y, num;
+		tie(x, y, num) = cctvs[k];
+		chosen[k] = { x, y, num, i };
+		func(k + 1);
+	}
+}
 
 int main()
 {
@@ -12,20 +103,14 @@ int main()
 	cin >> N >> M;
 	for (int i = 0; i < N; ++i)
 	{
-		cin >> a[i];
-		D[i] = a[i];
-		if (i != 0)
-			D[i] += D[i - 1];
+		for (int j = 0; j < M; ++j)
+		{
+			cin >> board[i][j];
+			if (board[i][j] > 0 && board[i][j] < 6)
+				cctvs.push_back({ i, j, board[i][j] });
+		}
 	}
 
-	while (M--)
-	{
-		int m;
-		cin >> m;
-
-		int i = lower_bound(D, D + N, m) - D;
-		if (D[i] == m)
-			++i;
-		cout << i << '\n';
-	}
+	func(0);
+	cout << ans;
 }
