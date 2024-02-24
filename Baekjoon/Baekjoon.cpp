6cc4130c71117d9d18a5c72f5b2094410dbbe617
hayoonleeMe <htdobad@gmail.com>
2024-02-24 16:32:51 +0900

    백준/14891

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,132 +1,60 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
-int dx[4] = { 0, 1, 0, -1 };
-int dy[4] = { 1, 0, -1, 0 };
+const int R = 2;
+const int L = 6;
 
-const int N = 12;
-const int M = 6;
-string board[12];
-bool vis[12][6];
-int ans = 0;
-
-void Drop()
-{
-	for (int j = 0; j < M; ++j)
-	{
-		for (int i = N - 1; i > 0; --i)
-		{
-			if (board[i][j] != '.')
-				continue;
-
-			int target = i - 1;
-			for (; target >= 0 && board[target][j] == '.'; --target);
-			if (target < 0)
-				break;
-			swap(board[i][j], board[target][j]);
-		}
-	}
-}
-
-bool CheckAndBoom()
-{
-	for (int i = 0; i < N; ++i)
-		fill(vis[i], vis[i] + M, 0);
-
-	bool bBoom = false;
-	for (int i = 0; i < N; ++i)
-	{
-		for (int j = 0; j < M; ++j)
-		{
-			if (board[i][j] == '.' || vis[i][j])
-				continue;
-
-			// Check
-			vector<pair<int, int>> v;
-			v.push_back({ i, j });
-			vis[i][j] = 1;
-			int cnt = 1;
-			int cur = 0;
-			while (cur < v.size())
-			{
-				pair<int, int> p = v[cur++];
-				for (int dir = 0; dir < 4; ++dir)
-				{
-					int nx = p.X + dx[dir];
-					int ny = p.Y + dy[dir];
-
-					if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-						continue;
-					if (board[nx][ny] != board[p.X][p.Y] || vis[nx][ny])
-						continue;
-
-					v.push_back({ nx, ny });
-					vis[nx][ny] = 1;
-					++cnt;
-				}
-			}
-
-			// Boom
-			if (cnt >= 4)
-			{
-				bBoom = true;
-				for (pair<int, int>& p : v)
-					board[p.X][p.Y] = '.';
-			}
-		}
-	}
-
-	return bBoom;
-}
+string t[5];
+int ans;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	for (int i = 0; i < N; ++i)
-		cin >> board[i];
-
-	for (int i = 0; i < N; ++i)
+	for (int i = 1; i <= 4; ++i)
+		cin >> t[i];
+	int T;
+	cin >> T;
+	while (T--)
 	{
-		for (int j = 0; j < M; ++j)
+		int n, d;
+		cin >> n >> d;
+
+		int dir[5] = { 0, };
+		dir[n] = d;
+
+		// 우측
+		for (int r = n + 1; r <= 4; ++r)
 		{
-			cout << board[i][j] << ' ';
+			if (t[r - 1][R] != t[r][L])
+				dir[r] = dir[r - 1] * -1;
+			else
+				break;
 		}
-		cout << '\n';
+
+		// 좌측
+		for (int l = n - 1; l >= 1; --l)
+		{
+			if (t[l + 1][L] != t[l][R])
+				dir[l] = dir[l + 1] * -1;
+			else
+				break;
+		}
+
+		// 회전
+		for (int i = 1; i <= 4; ++i)
+		{
+			if (dir[i] == 1)
+				rotate(t[i].begin(), t[i].begin() + 7, t[i].end());
+			else if (dir[i] == -1)
+				rotate(t[i].begin(), t[i].begin() + 1, t[i].end());
+		}
 	}
-	cout << '\n';
 
-	while (1)
+	for (int i = 1; i <= 4; ++i)
 	{
-		if (CheckAndBoom())
-			++ans;
-		else
-			break;
-
-		cout << "after checkandboom\n";
-		for (int i = 0; i < N; ++i)
-		{
-			for (int j = 0; j < M; ++j)
-			{
-				cout << board[i][j] << ' ';
-			}
-			cout << '\n';
-		}
-		cout << '\n';
-
-		Drop();
-		cout << "after drop\n";
-		for (int i = 0; i < N; ++i)
-		{
-			for (int j = 0; j < M; ++j)
-			{
-				cout << board[i][j] << ' ';
-			}
-			cout << '\n';
-		}
-		cout << '\n';
+		int m = (int)(t[i][0] - '0') << (i - 1);
+		ans += m;
 	}
 
 	cout << ans;
