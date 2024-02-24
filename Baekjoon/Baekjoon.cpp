a2013cca2fc998ee61d227e8ef3fdb2d7e25305c
hayoonleeMe <htdobad@gmail.com>
2024-02-24 14:46:00 +0900

    백준/11559

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,57 +1,133 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, W, L;
-int tr[1002];
-int br[1002];
+#define X first
+#define Y second
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { 1, 0, -1, 0 };
 
-bool IsBrEmpty()
+const int N = 12;
+const int M = 6;
+string board[12];
+bool vis[12][6];
+int ans = 0;
+
+void Drop()
 {
-	for (int i = 0; i < W; ++i)
-		if (br[i])
-			return false;
-	return true;
+	for (int j = 0; j < M; ++j)
+	{
+		for (int i = N - 1; i > 0; --i)
+		{
+			if (board[i][j] != '.')
+				continue;
+
+			int target = i - 1;
+			for (; target >= 0 && board[target][j] == '.'; --target);
+			if (target < 0)
+				break;
+			swap(board[i][j], board[target][j]);
+		}
+	}
+}
+
+bool CheckAndBoom()
+{
+	for (int i = 0; i < N; ++i)
+		fill(vis[i], vis[i] + M, 0);
+
+	bool bBoom = false;
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < M; ++j)
+		{
+			if (board[i][j] == '.' || vis[i][j])
+				continue;
+
+			// Check
+			vector<pair<int, int>> v;
+			v.push_back({ i, j });
+			vis[i][j] = 1;
+			int cnt = 1;
+			int cur = 0;
+			while (cur < v.size())
+			{
+				pair<int, int> p = v[cur++];
+				for (int dir = 0; dir < 4; ++dir)
+				{
+					int nx = p.X + dx[dir];
+					int ny = p.Y + dy[dir];
+
+					if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+						continue;
+					if (board[nx][ny] != board[p.X][p.Y] || vis[nx][ny])
+						continue;
+
+					v.push_back({ nx, ny });
+					vis[nx][ny] = 1;
+					++cnt;
+				}
+			}
+
+			// Boom
+			if (cnt >= 4)
+			{
+				bBoom = true;
+				for (pair<int, int>& p : v)
+					board[p.X][p.Y] = '.';
+			}
+		}
+	}
+
+	return bBoom;
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> W >> L;
 	for (int i = 0; i < N; ++i)
-		cin >> tr[i];
+		cin >> board[i];
 
-	int cur = 0;
-	int tot = 0;
-	int time = 0;
-	do
+	for (int i = 0; i < N; ++i)
 	{
-		// move
-		for (int i = W - 1; i >= 0; --i)
+		for (int j = 0; j < M; ++j)
 		{
-			if (br[i])
-			{
-				if (i + 1 >= W)
-					tot -= br[i];
-				else
-					br[i + 1] = br[i];
-				br[i] = 0;
-			}
+			cout << board[i][j] << ' ';
 		}
+		cout << '\n';
+	}
+	cout << '\n';
 
-		// add
-		if (cur < N && tot + tr[cur] <= L)
+	while (1)
+	{
+		if (CheckAndBoom())
+			++ans;
+		else
+			break;
+
+		cout << "after checkandboom\n";
+		for (int i = 0; i < N; ++i)
 		{
-			if (!br[0])
+			for (int j = 0; j < M; ++j)
 			{
-				br[0] = tr[cur];
-				tot += tr[cur];
-				++cur;
+				cout << board[i][j] << ' ';
 			}
+			cout << '\n';
 		}
+		cout << '\n';
 
-		++time;
-	} while (cur <= N && !IsBrEmpty());
+		Drop();
+		cout << "after drop\n";
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < M; ++j)
+			{
+				cout << board[i][j] << ' ';
+			}
+			cout << '\n';
+		}
+		cout << '\n';
+	}
 
-	cout << time;
+	cout << ans;
 }
