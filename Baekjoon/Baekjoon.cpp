3f5c89f20b29a580b0404c97fa5a6831aa7e924b
hayoonleeMe <htdobad@gmail.com>
2024-02-27 01:19:21 +0900

    백준/10974

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,39 +1,21 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, K;
-int a[10];
-vector<int> v;
-int ans;
-
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> K;
+	int N;
+	cin >> N;
+
+	vector<int> v;
 	for (int i = 0; i < N; ++i)
-	{
-		cin >> a[i];
-		v.push_back(i);
-	}
+		v.push_back(i + 1);
 
 	do
 	{
-		int w = 500;
-		bool can = true;
 		for (int i = 0; i < N; ++i)
-		{
-			w += -K + a[v[i]];
-			if (w < 500)
-			{
-				can = false;
-				break;
-			}
-		}
-		if (can)
-			++ans;
-
+			cout << v[i] << ' ';
+		cout << '\n';
 	} while (next_permutation(v.begin(), v.end()));
-
-	cout << ans;
 }
