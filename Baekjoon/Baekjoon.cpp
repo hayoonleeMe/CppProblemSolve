b0f0e18b04433bc4eb8291a618375d697f697dbb
hayoonleeMe <htdobad@gmail.com>
2024-02-27 01:18:38 +0900

    백준/29813

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,58 +1,29 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N, K;
-priority_queue<int, vector<int>> pq[13];
-int a[13];
+int N;
+deque<pair<string, int>> dq;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N >> K;
+	cin >> N;
 	for (int i = 0; i < N; ++i)
 	{
-		int a, b;
-		cin >> a >> b;
-		pq[a].push(b);
+		string s;
+		int n;
+		cin >> s >> n;
+		dq.push_back({ s, n });
 	}
 
-	for (int i = 1; i <= K; ++i)
+	while (dq.size() > 1)
 	{
-		for (int p = 1; p <= 11; ++p)
-		{
-			if (pq[p].empty())
-				continue;
-
-			if (a[p] < pq[p].top())
-			{
-				int t = a[p];
-				a[p] = pq[p].top();
-				pq[p].pop();
-				pq[p].push(t);
-			}
-		}
-
-		for (int p = 1; p <= 11; ++p)
-			a[p] = max(0, a[p] - 1);
-
-		for (int p = 1; p <= 11; ++p)
-		{
-			if (pq[p].empty())
-				continue;
-
-			if (a[p] < pq[p].top())
-			{
-				int t = a[p];
-				a[p] = pq[p].top();
-				pq[p].pop();
-				pq[p].push(t);
-			}
-		}
+		int cnt = (dq[0].second - 1) % (dq.size() - 1);
+		rotate(dq.begin() + 1, dq.begin() + 1 + cnt, dq.end());
+		dq.pop_front();
+		dq.pop_front();
 	}
 
-	int ans = 0;
-	for (int p = 1; p <= 11; ++p)
-		ans += a[p];
-	cout << ans;
+	cout << dq[0].first;
 }
