3181f64be3f95d543d13bbbb1fe2cd7718f0c39a
hayoonleeMe <htdobad@gmail.com>
2024-02-27 01:18:57 +0900

    백준/18429

--- Baekjoon/Baekjoon.cpp
+++ Baekjoon/Baekjoon.cpp
@@ -1,29 +1,39 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-deque<pair<string, int>> dq;
+int N, K;
+int a[10];
+vector<int> v;
+int ans;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
+	cin >> N >> K;
 	for (int i = 0; i < N; ++i)
 	{
-		string s;
-		int n;
-		cin >> s >> n;
-		dq.push_back({ s, n });
+		cin >> a[i];
+		v.push_back(i);
 	}
 
-	while (dq.size() > 1)
+	do
 	{
-		int cnt = (dq[0].second - 1) % (dq.size() - 1);
-		rotate(dq.begin() + 1, dq.begin() + 1 + cnt, dq.end());
-		dq.pop_front();
-		dq.pop_front();
-	}
+		int w = 500;
+		bool can = true;
+		for (int i = 0; i < N; ++i)
+		{
+			w += -K + a[v[i]];
+			if (w < 500)
+			{
+				can = false;
+				break;
+			}
+		}
+		if (can)
+			++ans;
 
-	cout << dq[0].first;
+	} while (next_permutation(v.begin(), v.end()));
+
+	cout << ans;
 }
